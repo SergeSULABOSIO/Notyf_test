4529a0622bace571eff981d293cf1f4659c65047
Serge - Bureau <sulabosiog@gmail.com>
2023-03-09 16:33:58 +0100

    SweetAlert en cours de construction...

--- assets/SweetAlert2.js
+++ assets/SweetAlert2.js
@@ -1,12 +1,47 @@
 // ES6 Modules or TypeScript
 import Swal from 'sweetalert2'
 
-// CommonJS
-//const Swal = require('sweetalert2')
 
-Swal.fire({
-  title: 'Error!',
-  text: 'Do you want to continue',
-  icon: 'error',
-  confirmButtonText: 'Cool'
-})
+
+document.getElementById("bt1").addEventListener("click", function () {
+  const Toast = Swal.mixin({
+    toast: true,
+    position: 'top-end',
+    showConfirmButton: false,
+    timer: 100000,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+      toast.addEventListener('mouseenter', Swal.stopTimer)
+      toast.addEventListener('mouseleave', Swal.resumeTimer)
+    }
+  })
+
+  Toast.fire({
+    icon: 'success',
+    title: 'Enregistrement effectué avec succès!'
+  })
+});
+
+
+document.getElementById("bt2").addEventListener("click", function () {
+  Swal.fire({
+    title: 'Suppression?',
+    text: "Etes-vous sûre de vouloir supprimer ceci?",
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Oui, supprimez-le'
+  }).then((result) => {
+    if (result.isConfirmed) {
+      Swal.fire(
+        'Supprimé avec succès',
+        'Enregistrement supprimé avec succès!',
+        'success'
+      )
+    }
+  })
+});
+
+
+

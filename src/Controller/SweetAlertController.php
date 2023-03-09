26c6d90593cbc4bbf5422579d5132eeab354f795
Serge - Bureau <sulabosiog@gmail.com>
2023-03-09 13:20:51 +0100

    SweetAlert en cours de construction...

---
+++ src/Controller/SweetAlertController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SweetAlertController extends AbstractController
+{
+    #[Route('/sweetalert', name: 'app_sweet_alert')]
+    public function index(): Response
+    {
+        
+        return $this->render('sweet_alert/index.html.twig', [
+            'controller_name' => 'SweetAlertController',
+        ]);
+    }
+}

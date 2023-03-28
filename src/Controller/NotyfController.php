9e75347e8c623276a135e8c36f53a9b550990696
Serge - Bureau <sulabosiog@gmail.com>
2023-03-28 12:44:32 +0100

    Restriction aux pages - OK

--- src/Controller/NotyfController.php
+++ src/Controller/NotyfController.php
@@ -5,10 +5,12 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class NotyfController extends AbstractController
 {
     #[Route('/notyf/{notyf}', name: 'app_notyf')]
+    #[IsGranted('ROLE_USER')]
     public function index(?string $notyf = null): Response
     {
 
--- src/Controller/SweetAlertController.php
+++ src/Controller/SweetAlertController.php
@@ -2,13 +2,15 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class SweetAlertController extends AbstractController
 {
     #[Route('/', name: 'app_sweet_alert')]
+    #[IsGranted('ROLE_USER')]
     public function index(): Response
     {
         

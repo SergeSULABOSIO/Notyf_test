4313d3f03d0852d9270a9422c7e3caaa9ba73da7
Serge SULA <sulabosiog@gmail.com>
2023-04-05 23:22:56 +0100

    Travail sur les Tests - Encours...

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -7,7 +7,6 @@ use App\Entity\EntreeStock;
 use App\Entity\Utilisateur;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use EasyCorp\Bundle\EasyAdminBundle\Config\Locale;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;

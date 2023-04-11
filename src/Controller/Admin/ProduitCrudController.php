885b46ffa1c6871977ea896e631b6aa061622f5b
Serge SULA <sulabosiog@gmail.com>
2023-04-11 17:41:54 +0100

    CRUD Produit - OK

--- src/Controller/Admin/ProduitCrudController.php
+++ src/Controller/Admin/ProduitCrudController.php
@@ -3,23 +3,113 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Produit;
+use Doctrine\ORM\EntityManagerInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Field\UrlField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TelephoneField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class ProduitCrudController extends AbstractCrudController
 {
+    public const ACTION_DUPLICATE = "Dupliquer";
+    public const ACTION_OPEN = "Ouvrir";
+
     public static function getEntityFqcn(): string
     {
         return Produit::class;
     }
 
-    /*
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setDateTimeFormat ('dd/MM/yyyy HH:mm:ss')
+            ->setDateFormat ('dd/MM/yyyy')
+            ->setPaginatorPageSize(30)
+            ->renderContentMaximized()
+            ->setEntityLabelInSingular("Produit")
+            ->setEntityLabelInPlural("Produits")
+            ->setPageTitle("index", "Liste des produits")
+            // ...
+        ;
+    }
+
+    
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            TextField::new('code', "Code"),
+            TextField::new('nom', "Intitulé"),
+            TextEditorField::new('description', "Description"),
+            NumberField::new('tauxarca', "Taux/Com. (%)"),
+            BooleanField::new('isobligatoire', "Obligatoire?"),
+            BooleanField::new('isabonnement', "Abonnement?"),
+            NumberField::new('categorie', "Catégorie")->hideOnIndex(),
+            AssociationField::new('entreprise', "Entreprise")->hideOnIndex(),
+            DateTimeField::new('createdAt', "Created at")->hideOnIndex(),
+            DateTimeField::new('updatedAt', "Updated at")
         ];
     }
-    */
+    
+    public function configureActions(Actions $actions): Actions
+    {
+        $duplicate = Action::new(self::ACTION_DUPLICATE)
+            ->linkToCrudAction('dupliquerEntite');//->setCssClass("btn btn-warning");
+
+        $ouvrir = Action::new(self::ACTION_OPEN)
+            ->linkToCrudAction('ouvrirEntite');
+
+        return $actions
+        //Action ouvrir
+        ->add(Crud::PAGE_EDIT, $ouvrir)
+        ->add(Crud::PAGE_INDEX, $ouvrir)
+        //action dupliquer Assureur
+        ->add(Crud::PAGE_DETAIL, $duplicate)
+        ->add(Crud::PAGE_EDIT, $duplicate)
+        ->add(Crud::PAGE_INDEX, $duplicate)
+        ->reorder(Crud::PAGE_INDEX, [self::ACTION_OPEN, self::ACTION_DUPLICATE])
+        ->reorder(Crud::PAGE_EDIT, [self::ACTION_OPEN, self::ACTION_DUPLICATE]);
+    }
+    
+    public function dupliquerEntite(AdminContext $context, AdminUrlGenerator $adminUrlGenerator, EntityManagerInterface $em)
+    {
+        /**@var Assureur $assureur */
+        $entite = $context->getEntity()->getInstance();
+        $entiteDuplique = clone $entite;
+        $this->parent::persistEntity($em, $entiteDuplique);
+
+        $url = $adminUrlGenerator
+            ->setController(self::class)
+            ->setAction(Action::DETAIL)
+            ->setEntityId($entiteDuplique->getId())
+            ->generateUrl();
+
+        return $this->redirect($url);
+    }
+
+    public function ouvrirEntite(AdminContext $context, AdminUrlGenerator $adminUrlGenerator, EntityManagerInterface $em)
+    {
+        /**@var Assureur $assureur */
+        $entite = $context->getEntity()->getInstance();
+        
+        $url = $adminUrlGenerator
+            ->setController(self::class)
+            ->setAction(Action::DETAIL)
+            ->setEntityId($entite->getId())
+            ->generateUrl();
+
+        return $this->redirect($url);
+    }
 }

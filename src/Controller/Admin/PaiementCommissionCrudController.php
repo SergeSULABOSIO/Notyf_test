5ffc32b3f672d4f3f3e8d76c5e6ec0543f4a1897
Serge SULA <sulabosiog@gmail.com>
2023-04-12 21:54:57 +0100

    CRUD - Monnaies - OK

--- src/Controller/Admin/PaiementCommissionCrudController.php
+++ src/Controller/Admin/PaiementCommissionCrudController.php
@@ -3,10 +3,29 @@
 namespace App\Controller\Admin;
 
 use App\Entity\PaiementCommission;
+use Doctrine\ORM\EntityManagerInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Field\UrlField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TelephoneField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class PaiementCommissionCrudController extends AbstractCrudController
 {
+    public const ACTION_DUPLICATE = "Dupliquer";
+    public const ACTION_OPEN = "Ouvrir";
+    
     public static function getEntityFqcn(): string
     {
         return PaiementCommission::class;

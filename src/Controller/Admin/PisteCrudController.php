c01b2de0e452dffb1a791d6e7f2dc6c4b2c04d7a
Serge SULA <sulabosiog@gmail.com>
2023-04-13 23:20:21 +0100

    Filtre pour le CRM - OK

--- src/Controller/Admin/PisteCrudController.php
+++ src/Controller/Admin/PisteCrudController.php
@@ -8,9 +8,11 @@ use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
 use EasyCorp\Bundle\EasyAdminBundle\Field\UrlField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
@@ -19,7 +21,6 @@ use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
-use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
 
 class PisteCrudController extends AbstractCrudController
 {
@@ -31,6 +32,17 @@ class PisteCrudController extends AbstractCrudController
         return Piste::class;
     }
 
+    public function configureFilters(Filters $filters): Filters
+    {
+        return $filters
+            ->add('contact')
+            ->add('utilisateur')
+            ->add('expiredAt')
+            ->add('etape')
+            ->add('cotations')
+            ->add('actions')
+        ;
+    }
 
     public function configureCrud(Crud $crud): Crud
     {

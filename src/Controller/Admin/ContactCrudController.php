b4174cc7dc9683aa20d675f9ac391d36e49f9c1c
Serge SULA - Bureau <sulabosiog@gmail.com>
2023-04-18 13:35:37 +0100

    Amélioration formulaires - Contact - OK

--- src/Controller/Admin/ContactCrudController.php
+++ src/Controller/Admin/ContactCrudController.php
@@ -11,9 +11,11 @@ use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
 use EasyCorp\Bundle\EasyAdminBundle\Field\UrlField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\FormField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
+use EasyCorp\Bundle\EasyAdminBundle\Dto\BatchActionDto;
 use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
@@ -25,10 +27,6 @@ use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class ContactCrudController extends AbstractCrudController
 {
-
-    public const ACTION_DUPLICATE = "Dupliquer";
-    public const ACTION_OPEN = "Ouvrir";
-
     public static function getEntityFqcn(): string
     {
         return Contact::class;
@@ -56,15 +54,83 @@ class ContactCrudController extends AbstractCrudController
         ;
     }
 
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            FormField::addPanel('Informations générales')
+            ->setIcon('fas fa-address-book') //<i class="fa-sharp fa-solid fa-address-book"></i>
+            ->setHelp("Tout simple un contact au sens littéral du terme. Une personne à contacter dans le cadre des assurances."),
+
+            //Ligne 01
+            TextField::new('nom', "Nom")->setColumns(6),
+            TextField::new('poste', "Poste")->setColumns(6),
+
+            //Ligne 02
+            TelephoneField::new('telephone', "Téléphone")->setColumns(6),
+            EmailField::new('email', "Email")->setColumns(6),
+
+            //Ligne 03
+            AssociationField::new('client', "Client")->setColumns(6),
+            DateTimeField::new('createdAt', "Date création")->hideOnForm()->setColumns(6),
+
+            //Ligne 04
+            DateTimeField::new('updatedAt', "Dernière modification")->hideOnForm(),
+            AssociationField::new('entreprise', "Entreprise")->hideOnIndex()
+        ];
+    }
+
     public function configureActions(Actions $actions): Actions
     {
-        $duplicate = Action::new(self::ACTION_DUPLICATE)
-            ->linkToCrudAction('dupliquerEntite');//->setCssClass("btn btn-warning");
-
-        $ouvrir = Action::new(self::ACTION_OPEN)
-            ->linkToCrudAction('ouvrirEntite');
+        $duplicate = Action::new(DashboardController::ACTION_DUPLICATE)->setIcon('fa-solid fa-copy')
+            ->linkToCrudAction('dupliquerEntite');//<i class="fa-solid fa-copy"></i>
+        $ouvrir = Action::new(DashboardController::ACTION_OPEN)
+            ->setIcon('fa-solid fa-eye')->linkToCrudAction('ouvrirEntite');//<i class="fa-solid fa-eye"></i>
+        $exporter_ms_excels = Action::new("exporter_ms_excels", DashboardController::ACTION_EXPORTER_EXCELS)
+            ->linkToCrudAction('exporterMSExcels')
+            ->addCssClass('btn btn-primary')
+            ->setIcon('fa-solid fa-file-excel');//<i class="fa-solid fa-file-excel"></i>
 
         return $actions
+        //Sur la page Index - Selection
+        ->addBatchAction($exporter_ms_excels)
+        //les Updates sur la page détail
+        ->update(Crud::PAGE_DETAIL, Action::DELETE, function (Action $action) {
+            return $action->setIcon('fa-solid fa-trash')->setLabel(DashboardController::ACTION_SUPPRIMER);
+        })
+        ->update(Crud::PAGE_DETAIL, Action::EDIT, function (Action $action) {
+            return $action->setIcon('fa-solid fa-pen-to-square')->setLabel(DashboardController::ACTION_MODIFIER);//<i class="fa-solid fa-pen-to-square"></i>
+        })
+        ->update(Crud::PAGE_DETAIL, Action::INDEX, function (Action $action) {
+            return $action->setIcon('fa-regular fa-rectangle-list')->setLabel(DashboardController::ACTION_LISTE);//<i class="fa-regular fa-rectangle-list"></i>
+        })
+        //Updates sur la page Index
+        ->update(Crud::PAGE_INDEX, Action::NEW, function (Action $action) {
+            return $action->setIcon('fas fa-address-book')->setCssClass('btn btn-primary')->setLabel(DashboardController::ACTION_AJOUTER);
+        })
+        ->update(Crud::PAGE_INDEX, Action::DELETE, function (Action $action) {
+            return $action->setIcon('fa-solid fa-trash')->setLabel(DashboardController::ACTION_SUPPRIMER);//<i class="fa-solid fa-trash"></i>
+        })
+        ->update(Crud::PAGE_INDEX, Action::BATCH_DELETE, function (Action $action) {
+            return $action->setIcon('fa-solid fa-trash')->setLabel(DashboardController::ACTION_SUPPRIMER);//<i class="fa-solid fa-trash"></i>
+        })
+        ->update(Crud::PAGE_INDEX, Action::EDIT, function (Action $action) {
+            return $action->setIcon('fa-solid fa-pen-to-square')->setLabel(DashboardController::ACTION_MODIFIER);
+        })
+        //Updates Sur la page Edit
+        ->update(Crud::PAGE_EDIT, Action::SAVE_AND_RETURN, function (Action $action) {
+            return $action->setIcon('fa-solid fa-floppy-disk')->setLabel(DashboardController::ACTION_ENREGISTRER);//<i class="fa-solid fa-floppy-disk"></i>
+        })
+        ->update(Crud::PAGE_EDIT, Action::SAVE_AND_CONTINUE, function (Action $action) {
+            return $action->setIcon('fa-solid fa-floppy-disk')->setLabel(DashboardController::ACTION_ENREGISTRER_ET_CONTINUER);
+        })
+        //Updates Sur la page NEW
+        ->update(Crud::PAGE_NEW, Action::SAVE_AND_ADD_ANOTHER, function (Action $action) {
+            return $action->setIcon('fa-solid fa-floppy-disk')->setLabel(DashboardController::ACTION_ENREGISTRER_ET_CONTINUER);
+        })
+        ->update(Crud::PAGE_NEW, Action::SAVE_AND_RETURN, function (Action $action) {
+            return $action->setIcon('fa-solid fa-floppy-disk')->setLabel(DashboardController::ACTION_ENREGISTRER);//<i class="fa-solid fa-floppy-disk"></i>
+        })
+
         //Action ouvrir
         ->add(Crud::PAGE_EDIT, $ouvrir)
         ->add(Crud::PAGE_INDEX, $ouvrir)
@@ -72,8 +138,9 @@ class ContactCrudController extends AbstractCrudController
         ->add(Crud::PAGE_DETAIL, $duplicate)
         ->add(Crud::PAGE_EDIT, $duplicate)
         ->add(Crud::PAGE_INDEX, $duplicate)
-        ->reorder(Crud::PAGE_INDEX, [self::ACTION_OPEN, self::ACTION_DUPLICATE])
-        ->reorder(Crud::PAGE_EDIT, [self::ACTION_OPEN, self::ACTION_DUPLICATE]);
+        //Reorganisation des boutons
+        ->reorder(Crud::PAGE_INDEX, [DashboardController::ACTION_OPEN, DashboardController::ACTION_DUPLICATE])
+        ->reorder(Crud::PAGE_EDIT, [DashboardController::ACTION_OPEN, DashboardController::ACTION_DUPLICATE]);
     }
     
     public function dupliquerEntite(AdminContext $context, AdminUrlGenerator $adminUrlGenerator, EntityManagerInterface $em)
@@ -106,19 +173,21 @@ class ContactCrudController extends AbstractCrudController
         return $this->redirect($url);
     }
 
-    
-    public function configureFields(string $pageName): iterable
+    public function exporterMSExcels(BatchActionDto $batchActionDto)
     {
-        return [
-            TextField::new('nom', "Nom"),
-            TextField::new('poste', "Poste"),
-            TelephoneField::new('telephone', "Téléphone"),
-            EmailField::new('email', "Email"),
-            AssociationField::new('client', "Client"),
-            DateTimeField::new('createdAt', "Date création")->hideOnForm(),
-            DateTimeField::new('updatedAt', "Dernière modification")->hideOnForm(),
-            AssociationField::new('entreprise', "Entreprise")->hideOnIndex()
-        ];
-    }
+        $className = $batchActionDto->getEntityFqcn();
+        $entityManager = $this->container->get('doctrine')->getManagerForClass($className);
+
+        dd($batchActionDto->getEntityIds());
+
+        foreach ($batchActionDto->getEntityIds() as $id) {
+            $user = $entityManager->find($className, $id);
+            $user->approve();
+        }
+
+        $entityManager->flush();
+
+        return $this->redirect($batchActionDto->getReferrerUrl());
+    }   
     
 }

b478a2ee8e7998fa547fa2fad0c9cc53d0bc9f6d
Serge SULA - Bureau <sulabosiog@gmail.com>
2023-05-09 14:21:18 +0100

    Visibilité des données - Etapes Sinistre - OK

--- src/Controller/Admin/EtapeSinistreCrudController.php
+++ src/Controller/Admin/EtapeSinistreCrudController.php
@@ -4,6 +4,9 @@ namespace App\Controller\Admin;
 
 use App\Entity\EtapeSinistre;
 use Doctrine\ORM\EntityManagerInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Collection\FieldCollection;
+use EasyCorp\Bundle\EasyAdminBundle\Collection\FilterCollection;
+use Doctrine\ORM\QueryBuilder;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
@@ -27,6 +30,8 @@ use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Dto\SearchDto;
+use EasyCorp\Bundle\EasyAdminBundle\Dto\EntityDto;
 
 class EtapeSinistreCrudController extends AbstractCrudController
 {
@@ -57,6 +62,19 @@ class EtapeSinistreCrudController extends AbstractCrudController
             ->setEntityPermission(UtilisateurCrudController::TAB_ROLES[UtilisateurCrudController::ACCES_SINISTRES])
             // ...
         ;
+    }
+
+    public function createIndexQueryBuilder(SearchDto $searchDto, EntityDto $entityDto, FieldCollection $fields, FilterCollection $filters): QueryBuilder
+    {
+        //dd($this->getUser());
+        $hasVisionGlobale = $this->isGranted(UtilisateurCrudController::TAB_ROLES[UtilisateurCrudController::VISION_GLOBALE]);
+        $defaultQueryBuilder = parent::createIndexQueryBuilder($searchDto, $entityDto, $fields, $filters);
+        if ($hasVisionGlobale) {
+            return $defaultQueryBuilder;
+        }
+        return $defaultQueryBuilder
+            ->andWhere('entity.utilisateur = :user')
+            ->setParameter('user', $this->getUser());
     }
 
     public function configureFilters(Filters $filters): Filters

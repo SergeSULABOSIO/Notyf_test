22e7cb799465893d32e6271c0506df6462d135c7
Serge SULA - Bureau <sulabosiog@gmail.com>
2023-05-15 16:51:44 +0100

    Filtrage Entreprise + utilisateur...

--- src/Controller/Admin/DocClasseurCrudController.php
+++ src/Controller/Admin/DocClasseurCrudController.php
@@ -86,6 +86,22 @@ class DocClasseurCrudController extends AbstractCrudController
         ;
     }
 
+    public function deleteEntity(EntityManagerInterface $entityManager, $entityInstance): void
+    {
+        //C'est dans cette méthode qu'il faut préalablement supprimer les enregistrements fils/déscendant de cette instance pour éviter l'erreur due à la contrainte d'intégrité
+        //dd($entityInstance);
+    }
+
+
+    public function createEntity(string $entityFqcn)
+    {
+        $objet = new DocClasseur();
+        //$objet->setStartedAt(new DateTimeImmutable("+1 day"));
+        //$objet->setEndedAt(new DateTimeImmutable("+7 day"));
+        //$objet->setClos(0);
+        return $objet;
+    }
+
     public function configureFields(string $pageName): iterable
     {
         return [
@@ -103,7 +119,7 @@ class DocClasseurCrudController extends AbstractCrudController
             //Ligne 02
             DateTimeField::new('createdAt', 'Date création')->hideOnForm(),
             DateTimeField::new('updatedAt', 'Dernière modification')->hideOnForm(),
-            AssociationField::new('entreprise', 'Entreprise')->hideOnIndex()->setColumns(6)
+            //AssociationField::new('entreprise', 'Entreprise')->hideOnIndex()->setColumns(6)
         ];
     }
 

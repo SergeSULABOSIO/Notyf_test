13b2f2b79e3593d9bbf00b0263aac57dcf56050c
Serge SULA - Bureau <sulabosiog@gmail.com>
2023-05-16 09:23:18 +0100

    Filtrage Entreprise + utilisateur...

--- src/Controller/Admin/MonnaieCrudController.php
+++ src/Controller/Admin/MonnaieCrudController.php
@@ -93,6 +93,22 @@ class MonnaieCrudController extends AbstractCrudController
         ;
     }
 
+    public function deleteEntity(EntityManagerInterface $entityManager, $entityInstance): void
+    {
+        //C'est dans cette méthode qu'il faut préalablement supprimer les enregistrements fils/déscendant de cette instance pour éviter l'erreur due à la contrainte d'intégrité
+        //dd($entityInstance);
+    }
+
+
+    public function createEntity(string $entityFqcn)
+    {
+        $objet = new Monnaie();
+        //$objet->setStartedAt(new DateTimeImmutable("+1 day"));
+        //$objet->setEndedAt(new DateTimeImmutable("+7 day"));
+        //$objet->setClos(0);
+        return $objet;
+    }
+
     public function configureFields(string $pageName): iterable
     {
         return [
@@ -115,7 +131,7 @@ class MonnaieCrudController extends AbstractCrudController
             //Ligne 03
             DateTimeField::new('createdAt', 'Date création')->hideOnIndex()->hideOnForm(),
             DateTimeField::new('updatedAt', 'Dernière modification')->hideOnForm(),
-            AssociationField::new('entreprise', 'Entreprise')->hideOnIndex()->setColumns(6)
+            //AssociationField::new('entreprise', 'Entreprise')->hideOnIndex()->setColumns(6)
         ];
     }
 

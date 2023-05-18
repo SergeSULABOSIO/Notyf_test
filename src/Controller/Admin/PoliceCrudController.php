673bc1201b41bf612899486d74692477911c73e9
Serge SULA - Bureau <sulabosiog@gmail.com>
2023-05-18 15:36:59 +0100

    Variables calculables / Police...

--- src/Controller/Admin/PoliceCrudController.php
+++ src/Controller/Admin/PoliceCrudController.php
@@ -323,6 +323,35 @@ class PoliceCrudController extends AbstractCrudController
             //->onlyOnDetail()
             ,
 
+            //LES CHAMPS CALCULABLES
+    //SECTION - REVENU
+    NumberField::new('calc_revenu_ht = 0;
+    NumberField::new('calc_revenu_ttc = 0;
+    NumberField::new('calc_revenu_ttc_encaisse = 0;
+    public $calc_revenu_ttc_encaisse_tab_ref_factures = [];
+    public $calc_revenu_ttc_encaisse_tab_dates = [];
+    public $calc_revenu_ttc_solde_restant_du = 0;
+    
+    //SECTION - PARTENAIRES
+    public $calc_retrocom = 0;
+    public $calc_retrocom_payees = 0;
+    public $calc_retrocom_payees_tab_factures = [];
+    public $calc_retrocom_payees_tab_dates = [];
+    public $calc_retrocom_solde = 0;
+
+    //SECTION - TAXES
+    public $calc_taxes_courtier = 0;
+    public $calc_taxes_courtier_payees = 0;
+    public $calc_taxes_courtier_payees_tab_ref_factures = [];
+    public $calc_taxes_courtier_payees_tab_dates = [];
+    public $calc_taxes_courtier_solde = 0;
+
+    public $calc_taxes_assureurs = 0;
+    public $calc_taxes_assureurs_payees = 0;
+    public $calc_taxes_assureurs_payees_tab_ref_factures = [];
+    public $calc_taxes_assureurs_payees_tab_dates = [];
+    public $calc_taxes_assureurs_solde = 0;
+
 
             FormField::addTab(' Documents / Pièces Justificatives')->setIcon('fas fa-file-shield'), 
             AssociationField::new('pieces', "Documents / pièces justificatives")->setColumns(12)->onlyOnForms()

e62e3ddd3eb5db9e2466234b50218cda79f1ef2d
Serge SULA - Bureau <sulabosiog@gmail.com>
2023-05-29 17:04:47 +0100

    Suppression / CRM / Piste

--- src/Controller/Admin/PisteCrudController.php
+++ src/Controller/Admin/PisteCrudController.php
@@ -8,6 +8,7 @@ use Doctrine\ORM\QueryBuilder;
 use App\Service\ServiceEntreprise;
 use Doctrine\ORM\EntityRepository;
 use App\Service\ServiceCalculateur;
+use App\Service\ServiceSuppression;
 use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
@@ -37,7 +38,12 @@ use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class PisteCrudController extends AbstractCrudController
 {
-    public function __construct(private ServiceCalculateur $serviceCalculateur, private EntityManagerInterface $entityManager, private ServiceEntreprise $serviceEntreprise)
+    public function __construct(
+        private ServiceSuppression $serviceSuppression,
+        private ServiceCalculateur $serviceCalculateur, 
+        private EntityManagerInterface $entityManager, 
+        private ServiceEntreprise $serviceEntreprise
+        )
     {
         
     }
@@ -98,8 +104,7 @@ class PisteCrudController extends AbstractCrudController
 
     public function deleteEntity(EntityManagerInterface $entityManager, $entityInstance): void
     {
-        //C'est dans cette méthode qu'il faut préalablement supprimer les enregistrements fils/déscendant de cette instance pour éviter l'erreur due à la contrainte d'intégrité
-        //dd($entityInstance);
+        $this->serviceSuppression->supprimer($entityInstance, ServiceSuppression::CRM_PISTE);
     }
 
 
--- src/Service/ServiceSuppression.php
+++ src/Service/ServiceSuppression.php
@@ -138,6 +138,10 @@ class ServiceSuppression
                 $this->supprimerEntiteSingleton($entityObject);
                 break;
 
+            case self::CRM_PISTE:
+                $this->supprimerEntiteSingleton($entityObject);
+                break;
+
             default:
                 dd("Cette fonction n'est pas encore disponible.");
                 break;

3d415d2e9c1fa8e63a1ae4221e2f343128c3e17a
Serge SULA - Bureau <sulabosiog@gmail.com>
2023-07-26 12:54:23 +0100

    Cross Canal/PaiementTaxe - Police

--- src/Controller/Admin/PaiementTaxeCrudController.php
+++ src/Controller/Admin/PaiementTaxeCrudController.php
@@ -43,19 +43,16 @@ class PaiementTaxeCrudController extends AbstractCrudController
 
     private ?Crud $crud = null;
 
-    public function __construct
-    (
-        private EntityManagerInterface $entityManager, 
+    public function __construct(
+        private EntityManagerInterface $entityManager,
         private ServiceEntreprise $serviceEntreprise,
         private ServiceSuppression $serviceSuppression,
-        private ServicePreferences $servicePreferences ,
+        private ServicePreferences $servicePreferences,
         private ServiceCrossCanal $serviceCrossCanal,
-        private AdminUrlGenerator $adminUrlGenerator       
-    )
-    {
-        
+        private AdminUrlGenerator $adminUrlGenerator
+    ) {
     }
-    
+
     public static function getEntityFqcn(): string
     {
         return PaiementTaxe::class;
@@ -66,8 +63,8 @@ class PaiementTaxeCrudController extends AbstractCrudController
         //Application de la préférence sur la taille de la liste
         $this->servicePreferences->appliquerPreferenceTaille(new PaiementTaxe(), $crud);
         $this->crud = $crud
-            ->setDateTimeFormat ('dd/MM/yyyy à HH:mm:ss')
-            ->setDateFormat ('dd/MM/yyyy')
+            ->setDateTimeFormat('dd/MM/yyyy à HH:mm:ss')
+            ->setDateFormat('dd/MM/yyyy')
             //->setPaginatorPageSize(100)
             ->renderContentMaximized()
             ->setEntityLabelInSingular("Taxe payée")
@@ -82,15 +79,14 @@ class PaiementTaxeCrudController extends AbstractCrudController
 
     public function configureFilters(Filters $filters): Filters
     {
-        if($this->isGranted(UtilisateurCrudController::TAB_ROLES[UtilisateurCrudController::VISION_GLOBALE])){
+        if ($this->isGranted(UtilisateurCrudController::TAB_ROLES[UtilisateurCrudController::VISION_GLOBALE])) {
             $filters->add('utilisateur');
         }
         return $filters
             ->add('montant')
             ->add('taxe')
             ->add('police')
-            ->add('pieces')
-        ;
+            ->add('pieces');
     }
 
     public function createIndexQueryBuilder(SearchDto $searchDto, EntityDto $entityDto, FieldCollection $fields, FilterCollection $filters): QueryBuilder
@@ -100,14 +96,12 @@ class PaiementTaxeCrudController extends AbstractCrudController
         $defaultQueryBuilder = parent::createIndexQueryBuilder($searchDto, $entityDto, $fields, $filters);
         if ($hasVisionGlobale == false) {
             $defaultQueryBuilder
-            ->Where('entity.utilisateur = :user')
-            ->setParameter('user', $this->getUser())
-            ;
+                ->Where('entity.utilisateur = :user')
+                ->setParameter('user', $this->getUser());
         }
         return $defaultQueryBuilder
             ->andWhere('entity.entreprise = :ese')
-            ->setParameter('ese', $connected_entreprise)
-        ;
+            ->setParameter('ese', $connected_entreprise);
     }
 
     public function deleteEntity(EntityManagerInterface $entityManager, $entityInstance): void
@@ -136,83 +130,84 @@ class PaiementTaxeCrudController extends AbstractCrudController
     public function configureActions(Actions $actions): Actions
     {
         $duplicate = Action::new(DashboardController::ACTION_DUPLICATE)->setIcon('fa-solid fa-copy')
-            ->linkToCrudAction('dupliquerEntite');//<i class="fa-solid fa-copy"></i>
+            ->linkToCrudAction('dupliquerEntite'); //<i class="fa-solid fa-copy"></i>
         $ouvrir = Action::new(DashboardController::ACTION_OPEN)
-            ->setIcon('fa-solid fa-eye')->linkToCrudAction('ouvrirEntite');//<i class="fa-solid fa-eye"></i>
+            ->setIcon('fa-solid fa-eye')->linkToCrudAction('ouvrirEntite'); //<i class="fa-solid fa-eye"></i>
         $exporter_ms_excels = Action::new("exporter_ms_excels", DashboardController::ACTION_EXPORTER_EXCELS)
             ->linkToCrudAction('exporterMSExcels')
             ->addCssClass('btn btn-primary')
             ->setIcon('fa-solid fa-file-excel');
 
         return $actions
-        //Sur la page Index - Selection
-        ->addBatchAction($exporter_ms_excels)
-        //les Updates sur la page détail
-        ->update(Crud::PAGE_DETAIL, Action::DELETE, function (Action $action) {
-            return $action->setIcon('fa-solid fa-trash')->setLabel(DashboardController::ACTION_SUPPRIMER);
-        })
-        ->update(Crud::PAGE_DETAIL, Action::EDIT, function (Action $action) {
-            return $action->setIcon('fa-solid fa-pen-to-square')->setLabel(DashboardController::ACTION_MODIFIER);//<i class="fa-solid fa-pen-to-square"></i>
-        })
-        ->update(Crud::PAGE_DETAIL, Action::INDEX, function (Action $action) {
-            return $action->setIcon('fa-regular fa-rectangle-list')->setLabel(DashboardController::ACTION_LISTE);//<i class="fa-regular fa-rectangle-list"></i>
-        })
-        //Updates sur la page Index
-        ->update(Crud::PAGE_INDEX, Action::NEW, function (Action $action) {
-            return $action->setIcon('fas fa-person-chalkboard')->setCssClass('btn btn-primary')->setLabel(DashboardController::ACTION_AJOUTER);
-        })
-        ->update(Crud::PAGE_INDEX, Action::DELETE, function (Action $action) {
-            return $action->setIcon('fa-solid fa-trash')->setLabel(DashboardController::ACTION_SUPPRIMER);//<i class="fa-solid fa-trash"></i>
-        })
-        ->update(Crud::PAGE_INDEX, Action::BATCH_DELETE, function (Action $action) {
-            return $action->setIcon('fa-solid fa-trash')->setLabel(DashboardController::ACTION_SUPPRIMER);//<i class="fa-solid fa-trash"></i>
-        })
-        ->update(Crud::PAGE_INDEX, Action::EDIT, function (Action $action) {
-            return $action->setIcon('fa-solid fa-pen-to-square')->setLabel(DashboardController::ACTION_MODIFIER);
-        })
-        //Updates Sur la page Edit
-        ->update(Crud::PAGE_EDIT, Action::SAVE_AND_RETURN, function (Action $action) {
-            return $action->setIcon('fa-solid fa-floppy-disk')->setLabel(DashboardController::ACTION_ENREGISTRER);//<i class="fa-solid fa-floppy-disk"></i>
-        })
-        ->update(Crud::PAGE_EDIT, Action::SAVE_AND_CONTINUE, function (Action $action) {
-            return $action->setIcon('fa-solid fa-floppy-disk')->setLabel(DashboardController::ACTION_ENREGISTRER_ET_CONTINUER);
-        })
-        //Updates Sur la page NEW
-        ->update(Crud::PAGE_NEW, Action::SAVE_AND_ADD_ANOTHER, function (Action $action) {
-            return $action->setIcon('fa-solid fa-floppy-disk')->setLabel(DashboardController::ACTION_ENREGISTRER_ET_CONTINUER);
-        })
-        ->update(Crud::PAGE_NEW, Action::SAVE_AND_RETURN, function (Action $action) {
-            return $action->setIcon('fa-solid fa-floppy-disk')->setLabel(DashboardController::ACTION_ENREGISTRER);//<i class="fa-solid fa-floppy-disk"></i>
-        })
+            //Sur la page Index - Selection
+            ->addBatchAction($exporter_ms_excels)
+            //les Updates sur la page détail
+            ->update(Crud::PAGE_DETAIL, Action::DELETE, function (Action $action) {
+                return $action->setIcon('fa-solid fa-trash')->setLabel(DashboardController::ACTION_SUPPRIMER);
+            })
+            ->update(Crud::PAGE_DETAIL, Action::EDIT, function (Action $action) {
+                return $action->setIcon('fa-solid fa-pen-to-square')->setLabel(DashboardController::ACTION_MODIFIER); //<i class="fa-solid fa-pen-to-square"></i>
+            })
+            ->update(Crud::PAGE_DETAIL, Action::INDEX, function (Action $action) {
+                return $action->setIcon('fa-regular fa-rectangle-list')->setLabel(DashboardController::ACTION_LISTE); //<i class="fa-regular fa-rectangle-list"></i>
+            })
+            //Updates sur la page Index
+            ->update(Crud::PAGE_INDEX, Action::NEW, function (Action $action) {
+                return $action->setIcon('fas fa-person-chalkboard')->setCssClass('btn btn-primary')->setLabel(DashboardController::ACTION_AJOUTER);
+            })
+            ->update(Crud::PAGE_INDEX, Action::DELETE, function (Action $action) {
+                return $action->setIcon('fa-solid fa-trash')->setLabel(DashboardController::ACTION_SUPPRIMER); //<i class="fa-solid fa-trash"></i>
+            })
+            ->update(Crud::PAGE_INDEX, Action::BATCH_DELETE, function (Action $action) {
+                return $action->setIcon('fa-solid fa-trash')->setLabel(DashboardController::ACTION_SUPPRIMER); //<i class="fa-solid fa-trash"></i>
+            })
+            ->update(Crud::PAGE_INDEX, Action::EDIT, function (Action $action) {
+                return $action->setIcon('fa-solid fa-pen-to-square')->setLabel(DashboardController::ACTION_MODIFIER);
+            })
+            //Updates Sur la page Edit
+            ->update(Crud::PAGE_EDIT, Action::SAVE_AND_RETURN, function (Action $action) {
+                return $action->setIcon('fa-solid fa-floppy-disk')->setLabel(DashboardController::ACTION_ENREGISTRER); //<i class="fa-solid fa-floppy-disk"></i>
+            })
+            ->update(Crud::PAGE_EDIT, Action::SAVE_AND_CONTINUE, function (Action $action) {
+                return $action->setIcon('fa-solid fa-floppy-disk')->setLabel(DashboardController::ACTION_ENREGISTRER_ET_CONTINUER);
+            })
+            //Updates Sur la page NEW
+            ->update(Crud::PAGE_NEW, Action::SAVE_AND_ADD_ANOTHER, function (Action $action) {
+                return $action->setIcon('fa-solid fa-floppy-disk')->setLabel(DashboardController::ACTION_ENREGISTRER_ET_CONTINUER);
+            })
+            ->update(Crud::PAGE_NEW, Action::SAVE_AND_RETURN, function (Action $action) {
+                return $action->setIcon('fa-solid fa-floppy-disk')->setLabel(DashboardController::ACTION_ENREGISTRER); //<i class="fa-solid fa-floppy-disk"></i>
+            })
 
-        //Action ouvrir
-        ->add(Crud::PAGE_EDIT, $ouvrir)
-        ->add(Crud::PAGE_INDEX, $ouvrir)
-        //action dupliquer Assureur
-        ->add(Crud::PAGE_DETAIL, $duplicate)
-        ->add(Crud::PAGE_EDIT, $duplicate)
-        ->add(Crud::PAGE_INDEX, $duplicate)
-        //Reorganisation des boutons
-        ->reorder(Crud::PAGE_INDEX, [DashboardController::ACTION_OPEN, DashboardController::ACTION_DUPLICATE])
-        ->reorder(Crud::PAGE_EDIT, [DashboardController::ACTION_OPEN, DashboardController::ACTION_DUPLICATE])
-        
-        //Application des roles
-        ->setPermission(Action::NEW, UtilisateurCrudController::TAB_ROLES[UtilisateurCrudController::ACTION_EDITION])
-        ->setPermission(Action::EDIT, UtilisateurCrudController::TAB_ROLES[UtilisateurCrudController::ACTION_EDITION])
-        ->setPermission(Action::DELETE, UtilisateurCrudController::TAB_ROLES[UtilisateurCrudController::ACTION_EDITION])
-        ->setPermission(Action::BATCH_DELETE, UtilisateurCrudController::TAB_ROLES[UtilisateurCrudController::ACTION_EDITION])
-        ->setPermission(Action::SAVE_AND_ADD_ANOTHER, UtilisateurCrudController::TAB_ROLES[UtilisateurCrudController::ACTION_EDITION])
-        ->setPermission(Action::SAVE_AND_CONTINUE, UtilisateurCrudController::TAB_ROLES[UtilisateurCrudController::ACTION_EDITION])
-        ->setPermission(Action::SAVE_AND_RETURN, UtilisateurCrudController::TAB_ROLES[UtilisateurCrudController::ACTION_EDITION])
-        ->setPermission(DashboardController::ACTION_DUPLICATE, UtilisateurCrudController::TAB_ROLES[UtilisateurCrudController::ACTION_EDITION])
-        //->setPermission(self::ACTION_ACHEVER_MISSION, UtilisateurCrudController::TAB_ROLES[UtilisateurCrudController::ACTION_EDITION])
-        //->setPermission(self::ACTION_AJOUTER_UN_FEEDBACK, UtilisateurCrudController::TAB_ROLES[UtilisateurCrudController::ACTION_EDITION])
-;
+            //Action ouvrir
+            ->add(Crud::PAGE_EDIT, $ouvrir)
+            ->add(Crud::PAGE_INDEX, $ouvrir)
+            //action dupliquer Assureur
+            ->add(Crud::PAGE_DETAIL, $duplicate)
+            ->add(Crud::PAGE_EDIT, $duplicate)
+            ->add(Crud::PAGE_INDEX, $duplicate)
+            //Reorganisation des boutons
+            ->reorder(Crud::PAGE_INDEX, [DashboardController::ACTION_OPEN, DashboardController::ACTION_DUPLICATE])
+            ->reorder(Crud::PAGE_EDIT, [DashboardController::ACTION_OPEN, DashboardController::ACTION_DUPLICATE])
+
+            //Application des roles
+            ->setPermission(Action::NEW, UtilisateurCrudController::TAB_ROLES[UtilisateurCrudController::ACTION_EDITION])
+            ->setPermission(Action::EDIT, UtilisateurCrudController::TAB_ROLES[UtilisateurCrudController::ACTION_EDITION])
+            ->setPermission(Action::DELETE, UtilisateurCrudController::TAB_ROLES[UtilisateurCrudController::ACTION_EDITION])
+            ->setPermission(Action::BATCH_DELETE, UtilisateurCrudController::TAB_ROLES[UtilisateurCrudController::ACTION_EDITION])
+            ->setPermission(Action::SAVE_AND_ADD_ANOTHER, UtilisateurCrudController::TAB_ROLES[UtilisateurCrudController::ACTION_EDITION])
+            ->setPermission(Action::SAVE_AND_CONTINUE, UtilisateurCrudController::TAB_ROLES[UtilisateurCrudController::ACTION_EDITION])
+            ->setPermission(Action::SAVE_AND_RETURN, UtilisateurCrudController::TAB_ROLES[UtilisateurCrudController::ACTION_EDITION])
+            ->setPermission(DashboardController::ACTION_DUPLICATE, UtilisateurCrudController::TAB_ROLES[UtilisateurCrudController::ACTION_EDITION])
+            //->setPermission(self::ACTION_ACHEVER_MISSION, UtilisateurCrudController::TAB_ROLES[UtilisateurCrudController::ACTION_EDITION])
+            //->setPermission(self::ACTION_AJOUTER_UN_FEEDBACK, UtilisateurCrudController::TAB_ROLES[UtilisateurCrudController::ACTION_EDITION])
+            ->remove(Crud::PAGE_INDEX, Action::NEW)
+            ;
     }
-    
+
     public function dupliquerEntite(AdminContext $context, AdminUrlGenerator $adminUrlGenerator, EntityManagerInterface $em)
     {
-        
+
         $entite = $context->getEntity()->getInstance();
         $entiteDuplique = clone $entite;
         parent::persistEntity($em, $entiteDuplique);
@@ -230,7 +225,7 @@ class PaiementTaxeCrudController extends AbstractCrudController
     {
         /**@var Assureur $assureur */
         $entite = $context->getEntity()->getInstance();
-                
+
         $url = $adminUrlGenerator
             ->setController(self::class)
             ->setAction(Action::DETAIL)
@@ -256,5 +251,4 @@ class PaiementTaxeCrudController extends AbstractCrudController
 
         return $this->redirect($batchActionDto->getReferrerUrl());
     }
-
 }

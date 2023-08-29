7005ba0619b3c1e8b4781a2ba72f8619a834ef4b
Serge SULA - Bureau <sulabosiog@gmail.com>
2023-08-29 12:33:53 +0100

    Facturation et encaissement des commissions

--- src/Service/ServiceFacture.php
+++ src/Service/ServiceFacture.php
@@ -12,40 +12,96 @@ use App\Entity\Cotation;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\SecurityBundle\Security;
 use App\Controller\Admin\PoliceCrudController;
+use App\Entity\ElementFacture;
 use App\Entity\Facture;
 use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use PhpParser\Node\Expr\Cast\Array_;
 
 class ServiceFacture
 {
     public function __construct(
         private ServiceDates $serviceDates,
+        private ServiceCalculateur $serviceCalculateur,
         private EntityManagerInterface $entityManager,
         private ServiceEntreprise $serviceEntreprise,
         private Security $security
     ) {
     }
 
-    public function initFature(Facture $facture, AdminUrlGenerator $adminUrlGenerator): Facture{
+    public function initFature(Facture $facture, AdminUrlGenerator $adminUrlGenerator): Facture
+    {
         $facture->setReference(strtoupper(str_replace(" ", "", "ND" . Date("dmYHis") . "/" . $this->serviceEntreprise->getEntreprise()->getNom() . "/" . Date("Y"))));
         $facture->setCreatedAt($this->serviceDates->aujourdhui());
         $facture->setUpdatedAt($this->serviceDates->aujourdhui());
         $facture->setUtilisateur($this->serviceEntreprise->getUtilisateur());
         $facture->setEntreprise($this->serviceEntreprise->getEntreprise());
-        if($adminUrlGenerator->get("donnees")){
+        if ($adminUrlGenerator->get("donnees")) {
             $data = $adminUrlGenerator->get("donnees");
             $description = "";
-            if($data["type"]){
+            if ($data["type"] && $data["tabPolices"]) {
                 $description = $data["type"] . "<br>Ref.:" . $facture->getReference();
-                $facture->setType(FactureCrudController::TAB_TYPE_FACTURE[$data["type"]]); //Date("dmYHis")
-            }
-            if($data["tabPolices"]){
                 $description = $description . "<br>" . count($data["tabPolices"]) . " élément(s).";
+                $facture->setType(FactureCrudController::TAB_TYPE_FACTURE[$data["type"]]);
+                $this->chargerElementFactures($facture, $data["type"], $data["tabPolices"]);
             }
             $facture->setDescription($description);
         }
-        
+
         //$facture->setType(self::TAB_TYPE_FACTURE[self::TYPE_FACTURE_COMMISSIONS]);
 
         return $facture;
+    }
+
+    private function chargerElementFactures(Facture $facture, $typeFacture, array $tabIdPolices)
+    {
+        foreach ($tabIdPolices as $idPolice) {
+            /** @var Police */
+            $oPolice = $this->entityManager->getRepository(Police::class)->find($idPolice);
+            if ($oPolice) {
+                $this->serviceCalculateur->updatePoliceCalculableFileds($oPolice);
+                switch ($typeFacture) {
+                    case FactureCrudController::TYPE_FACTURE_COMMISSIONS:
+                        /** @var ElementFacture */
+                        $ef = new ElementFacture();
+                        $ef->setPolice($oPolice);
+                        $ef->setMontant($oPolice->calc_revenu_ttc_solde_restant_du);
+                        break;
+                    case FactureCrudController::TYPE_FACTURE_RETROCOMMISSIONS:
+                        /** @var ElementFacture */
+                        $ef = new ElementFacture();
+                        $ef->setPolice($oPolice);
+                        $ef->setMontant($oPolice->calc_retrocom_solde);
+                        break;
+                    case FactureCrudController::TYPE_FACTURE_NOTE_DE_PERCEPTION_TVA:
+                        /** @var ElementFacture */
+                        $ef = new ElementFacture();
+                        $ef->setPolice($oPolice);
+                        $ef->setMontant($oPolice->calc_taxes_assureurs_solde);
+                        break;
+                    case FactureCrudController::TYPE_FACTURE_NOTE_DE_PERCEPTION_ARCA:
+                        /** @var ElementFacture */
+                        $ef = new ElementFacture();
+                        $ef->setPolice($oPolice);
+                        $ef->setMontant($oPolice->calc_taxes_courtier_solde);
+                        break;
+                    default:
+                        # code...
+                        break;
+                }
+            }
+            if ($ef->getMontant() != 0) {
+                $this->setAutresAttributs($facture, $ef);
+            }
+        }
+    }
+
+    private function setAutresAttributs(Facture $facture, ElementFacture $ef)
+    {
+        $ef->setEntreprise($this->serviceEntreprise->getEntreprise());
+        $ef->setUtilisateur($this->serviceEntreprise->getUtilisateur());
+        $ef->setCreatedAt($this->serviceDates->aujourdhui());
+        $ef->setUpdatedAt($this->serviceDates->aujourdhui());
+        $ef->setFacture($facture);
+        $facture->addElementFacture($ef);
     }
 }

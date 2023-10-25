6a4e2433954b57056d35f7b0c877f41e3f8cd9a8
Serge SULA - Bureau <sulabosiog@gmail.com>
2023-10-25 22:55:29 +0100

    Facturation et encaissement des commissions

--- src/Controller/Admin/PaiementCrudController.php
+++ src/Controller/Admin/PaiementCrudController.php
@@ -3,23 +3,116 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Paiement;
+use App\Service\ServiceDates;
+use App\Service\ServiceTaxes;
+use Doctrine\ORM\QueryBuilder;
+use App\Service\ServiceAvenant;
+use App\Service\ServiceMonnaie;
+use App\Service\ServiceCrossCanal;
+use App\Service\ServiceEntreprise;
+use App\Service\ServiceCalculateur;
+use App\Service\ServicePreferences;
+use App\Service\ServiceSuppression;
+use Doctrine\ORM\EntityManagerInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Dto\EntityDto;
+use EasyCorp\Bundle\EasyAdminBundle\Dto\SearchDto;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
+use EasyCorp\Bundle\EasyAdminBundle\Filter\ChoiceFilter;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use EasyCorp\Bundle\EasyAdminBundle\Collection\FieldCollection;
+use EasyCorp\Bundle\EasyAdminBundle\Collection\FilterCollection;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class PaiementCrudController extends AbstractCrudController
 {
+    public ?Paiement $paiement = null;
+    public ?Crud $crud = null;
+
+    public function __construct(
+        private ServiceMonnaie $serviceMonnaie,
+        private ServiceDates $serviceDates,
+        private ServiceAvenant $serviceAvenant,
+        private ServiceSuppression $serviceSuppression,
+        private ServiceCalculateur $serviceCalculateur,
+        private EntityManagerInterface $entityManager,
+        private ServiceEntreprise $serviceEntreprise,
+        private ServicePreferences $servicePreferences,
+        private ServiceCrossCanal $serviceCrossCanal,
+        private AdminUrlGenerator $adminUrlGenerator,
+        private ServiceTaxes $serviceTaxes
+    ) {
+        //$this->dompdf = new Dompdf();
+    }
+
+    
     public static function getEntityFqcn(): string
     {
         return Paiement::class;
     }
 
-    /*
-    public function configureFields(string $pageName): iterable
+    public function createIndexQueryBuilder(SearchDto $searchDto, EntityDto $entityDto, FieldCollection $fields, FilterCollection $filters): QueryBuilder
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+        $connected_entreprise = $this->serviceEntreprise->getEntreprise();
+        $hasVisionGlobale = $this->isGranted(UtilisateurCrudController::TAB_ROLES[UtilisateurCrudController::VISION_GLOBALE]);
+        $defaultQueryBuilder = parent::createIndexQueryBuilder($searchDto, $entityDto, $fields, $filters);
+        if ($hasVisionGlobale == false) {
+            $defaultQueryBuilder
+                ->Where('entity.utilisateur = :user')
+                ->setParameter('user', $this->getUser());
+        }
+        return $defaultQueryBuilder
+            ->andWhere('entity.entreprise = :ese')
+            ->setParameter('ese', $connected_entreprise);
     }
-    */
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        //Application de la préférence sur la taille de la liste
+        $this->servicePreferences->appliquerPreferenceTaille(new Paiement(), $crud);
+        $this->crud = $crud
+            ->setDateTimeFormat('dd/MM/yyyy à HH:mm:ss')
+            ->setDateFormat('dd/MM/yyyy')
+            ->setHelp(Crud::PAGE_NEW, "Founissez les informations recquises puis validez le formulaire pour les sauvegarder.")
+            ->setHelp(Crud::PAGE_INDEX, "Résultat du filtrage.")
+            ->setHelp(Crud::PAGE_DETAIL, "Information détailée sur l'enregistrement séléctioné.")
+            ->setHelp(Crud::PAGE_EDIT, "Mise à jour d'un enregistrement. N'oubliez pas de valider le formulaire.")
+            ->renderContentMaximized()
+            ->setEntityLabelInSingular("Paiement")
+            ->setEntityLabelInPlural("Paiements")
+            ->setPageTitle("index", "Liste des paiments")
+            ->setDefaultSort(['updatedAt' => 'DESC'])
+            ->setEntityPermission(UtilisateurCrudController::TAB_ROLES[UtilisateurCrudController::ACCES_FINANCES])
+            // ...
+        ;
+        return $crud;
+    }
+
+
+    public function configureFilters(Filters $filters): Filters
+    {
+        if ($this->isGranted(UtilisateurCrudController::TAB_ROLES[UtilisateurCrudController::VISION_GLOBALE])) {
+            $filters->add('utilisateur');
+        }
+        return $filters
+            ->add('facture')
+            ->add('paidAt')
+            ->add('createdAt')
+            ->add('updatedAt');
+    }
+
+    public function deleteEntity(EntityManagerInterface $entityManager, $entityInstance): void
+    {
+        $this->serviceSuppression->supprimer($entityInstance, ServiceSuppression::FINANCE_PAIEMENT);
+    }
+
+    public function createEntity(string $entityFqcn)
+    {
+        $objet = new Paiement();
+        $objet->setPaidAt(new \DateTimeImmutable("now"));
+        //$objet = $this->serviceCrossCanal->crossCanal_Police_setCotation($objet, $this->adminUrlGenerator);
+        //dd($objet);
+        return $objet;
+    }
+   
 }

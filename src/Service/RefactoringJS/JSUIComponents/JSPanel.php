82e425dd433a188814bcc16a988a1897aa3338e6
Serge SULA - Bureau <sulabosiog@gmail.com>
2024-01-22 13:32:34 +0100

    Application des collections - Finances - après prise en compte des collections

--- src/Service/RefactoringJS/JSUIComponents/JSPanel.php
+++ src/Service/RefactoringJS/JSUIComponents/JSPanel.php
@@ -5,7 +5,20 @@ namespace App\Service\RefactoringJS\JSUIComponents;
 
 interface JSPanel
 {
+    public const TYPE_LISTE = 0;
+    public const TYPE_DETAILS = 1;
+    public const TYPE_FORMULAIRE = 2;
+
     //Fonctions de creation du panel
-    public function addPanel(?string $titre);
+    public function setType(?int $type);
+    public function addSection(?string $titre, ?string $icone, ?int $colonne);
+    public function addOnglet(?string $titre, ?string $icone, ?string $helpMessage);
+    public function addChampAssociation(?string $attribut, ?string $titre, ?bool $required, ?bool $desabled, ?int $columns);
+    public function addChampChoix(?string $attribut, ?string $titre, ?bool $required, ?bool $desabled, ?int $columns, ?array $choices);
+    public function addChampArgent(?string $attribut, ?string $titre, ?bool $required, ?bool $desabled, ?int $columns, ?string $currency);
+    public function addChampDate(?string $attribut, ?string $titre, ?bool $required, ?bool $desabled, ?int $columns);
+    
+    public function getChamps():?array;
     public function reset();
+    public function init();
 }
---
+++ src/Service/RefactoringJS/JSUIComponents/JSPanelBuilder.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Service\RefactoringJS\JSUIComponents;
+
+use DateTimeImmutable;
+use App\Entity\Facture;
+use App\Entity\Tranche;
+use App\Entity\Assureur;
+use App\Entity\Entreprise;
+use App\Entity\Partenaire;
+use App\Entity\Utilisateur;
+use App\Entity\ElementFacture;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+
+abstract class JSPanelBuilder
+{
+    private ?array $champs;
+    private ?string $pageName;
+    private $objetInstance;
+    private ?Crud $crud;
+    private ?AdminUrlGenerator $adminUrlGenerator;
+
+
+    public function __construct()
+    {
+        $this->initChamps();
+    }
+
+    public abstract function buildListPanel(string $pageName, $objetInstance, $crud, AdminUrlGenerator $adminUrlGenerator): ?JSPanel;
+    public abstract function buildDetailsPanel(string $pageName, $objetInstance, $crud, AdminUrlGenerator $adminUrlGenerator): ?JSPanel;
+    public abstract function buildFormPanel(string $pageName, $objetInstance, $crud, AdminUrlGenerator $adminUrlGenerator): ?JSPanel;
+
+    private function initChamps(){
+        $this->champs = [];
+    }
+
+    public function render(string $pageName, $objetInstance, $crud, AdminUrlGenerator $adminUrlGenerator)
+    {
+        $this->pageName = $pageName;
+        $this->objetInstance = $objetInstance;
+        $this->crud = $crud;
+        $this->adminUrlGenerator = $adminUrlGenerator;
+        $this->initChamps();
+
+        switch ($this->pageName) {
+            case Crud::PAGE_INDEX:
+                $this->champs = $this->buildListPanel(
+                    $this->pageName,
+                    $this->objetInstance,
+                    $this->crud,
+                    $this->adminUrlGenerator
+                );
+                break;
+            case Crud::PAGE_DETAIL:
+                $this->champs = $this->buildDetailsPanel(
+                    $this->pageName,
+                    $this->objetInstance,
+                    $this->crud,
+                    $this->adminUrlGenerator
+                );
+                break;
+            case Crud::PAGE_EDIT:
+                $this->champs = $this->buildListPanel(
+                    $this->pageName,
+                    $this->objetInstance,
+                    $this->crud,
+                    $this->adminUrlGenerator
+                );
+                break;
+
+            default:
+                dd("Opération non prise en compte.");
+                break;
+        }
+        return $this->champs;
+    }
+}

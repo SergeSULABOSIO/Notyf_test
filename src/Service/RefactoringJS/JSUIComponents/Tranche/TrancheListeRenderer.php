ca6a5b079e3faa22503e3f1b291c2f52c4931d1c
Serge SULA - Bureau <sulabosiog@gmail.com>
2024-02-07 17:01:15 +0100

    Facturation et paiements - Pronema

--- src/Service/RefactoringJS/JSUIComponents/Tranche/TrancheListeRenderer.php
+++ src/Service/RefactoringJS/JSUIComponents/Tranche/TrancheListeRenderer.php
@@ -94,67 +94,6 @@ class TrancheListeRenderer extends JSPanelRenderer
             "admin/segment/index_tranche_status.html.twig"
         );
         Ici
-
-
-
-        /*************** */
-        $this->addChampChoix(
-            null,
-            "type",
-            "Type",
-            true,
-            true,
-            10,
-            PaiementCrudController::TAB_TYPE_PAIEMENT,
-            [
-                PaiementCrudController::TAB_TYPE_PAIEMENT[PaiementCrudController::TYPE_PAIEMENT_ENTREE] => 'success', //info
-                PaiementCrudController::TAB_TYPE_PAIEMENT[PaiementCrudController::TYPE_PAIEMENT_SORTIE] => 'danger', //info
-            ]
-        );
-        $this->addChampArgent(
-            null,
-            "montant",
-            "Montant",
-            true,
-            false,
-            10,
-            $this->serviceMonnaie->getCodeAffichage()
-        );
-        $this->addChampDate(
-            null,
-            "paidAt",
-            "Date de paiement",
-            false,
-            false,
-            10
-        );
-        $this->addChampAssociation(
-            null,
-            "facture",
-            "Facture",
-            false,
-            false,
-            10,
-            null
-        );
-        $this->addChampAssociation(
-            null,
-            "utilisateur",
-            "Utilisateur",
-            false,
-            false,
-            10,
-            null
-        );
-        $this->addChampAssociation(
-            null,
-            "entreprise",
-            "Entreprise",
-            false,
-            false,
-            10,
-            null
-        );
     }
 
     public function batchActions(?array $champs, ?string $type = null, ?string $pageName = null, $objetInstance = null, ?Crud $crud = null, ?AdminUrlGenerator $adminUrlGenerator = null): ?array

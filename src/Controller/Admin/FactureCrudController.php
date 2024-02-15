0e9753af1ee7fed997f23ecc8c648c1a1a7b64d9
Serge SULA - Bureau <sulabosiog@gmail.com>
2024-02-15 16:56:09 +0100

    Facturation et paiements - Pronema

--- src/Controller/Admin/FactureCrudController.php
+++ src/Controller/Admin/FactureCrudController.php
@@ -65,9 +65,9 @@ class FactureCrudController extends AbstractCrudController
     public const STATUS_FACTURE_SOLDEE      = "Soldée";
 
     public const TAB_STATUS_FACTURE = [
-        self::STATUS_FACTURE_IMPAYEE    => 0,
+        self::STATUS_FACTURE_IMPAYEE    => 2,
         self::STATUS_FACTURE_ENCOURS    => 1,
-        self::STATUS_FACTURE_SOLDEE     => 2,
+        self::STATUS_FACTURE_SOLDEE     => 0,
     ];
 
     public ?Crud $crud = null;
--- src/Service/RefactoringJS/Initialisateurs/Paiement/PaiementTaxeAssureurInit.php
+++ src/Service/RefactoringJS/Initialisateurs/Paiement/PaiementTaxeAssureurInit.php
@@ -19,12 +19,8 @@ use App\Controller\Admin\PaiementCrudController;
 use App\Service\ServiceTaxes;
 use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 
-class PaiementTaxeAssureurInit implements PaiementInit
+class PaiementTaxeAssureurInit extends AbstractPaiement
 {
-    // private ?Police $police;
-    private ?Facture $facture;
-    private ?Paiement $paiement;
-
     public function __construct(
         private AdminUrlGenerator $adminUrlGenerator,
         private ServiceAvenant $serviceAvenant,
@@ -34,180 +30,23 @@ class PaiementTaxeAssureurInit implements PaiementInit
         private EntityManagerInterface $entityManager,
         private ServiceCompteBancaire $serviceCompteBancaire
     ) {
-        $this->paiement = new Paiement();
+        parent::__construct(
+            $adminUrlGenerator,
+            $serviceAvenant,
+            $serviceDates,
+            $serviceEntreprise,
+            $entityManager,
+            $serviceCompteBancaire
+        );
     }
 
-    public function setPaidAt(?DateTimeImmutable $paidAt)
+    public function getTypePaiement(): ?string
     {
-        $this->paiement->setPaidAt($paidAt);
+        return PaiementCrudController::TYPE_PAIEMENT_SORTIE;
     }
 
-    public function setMontant(?float $paidAmount)
+    public function getNomAbstract(): ?string
     {
-        $this->paiement->setMontant($paidAmount);
-    }
-
-    public function setDescription(?string $description)
-    {
-        $this->paiement->setDescription($description);
-    }
-
-    public function setFacture(?Facture $facture)
-    {
-        $this->paiement->setFacture($facture);
-    }
-
-    public function setEntreprise(?Entreprise $entreprise)
-    {
-        $this->paiement->setEntreprise($entreprise);
-    }
-
-    public function setUtilisateur(?Utilisateur $utilisateur)
-    {
-        $this->paiement->setUtilisateur($utilisateur);
-    }
-
-    public function setCreatedAt(?DateTimeImmutable $dateCreation)
-    {
-        $this->paiement->setCreatedAt($dateCreation);
-    }
-
-    public function setUpdatedAt(?DateTimeImmutable $dateModification)
-    {
-        $this->paiement->setUpdatedAt($dateModification);
-    }
-
-    public function setCompteBancaire(?CompteBancaire $compteBancaire)
-    {
-        $this->paiement->setCompteBancaire($compteBancaire);
-    }
-
-    public function addDocument(?DocPiece $document)
-    {
-        $this->paiement->addDocument($document);
-    }
-
-    public function setType(?int $typePaiement)
-    {
-        $this->paiement->setType($typePaiement);
-    }
-
-    public function setTypeFacture(?int $typeFacture)
-    {
-        $this->paiement->setTypeFacture($typeFacture);
-    }
-
-    public function reset()
-    {
-        $this->paiement = null;
-    }
-
-    public function buildPaiement(?Facture $facture, ?DateTimeImmutable $dateOfPayment, ?Utilisateur $utilisateur, ?float $paidAmount): ?Paiement
-    {
-        if ($facture == null) {
-            /** @var Facture*/
-            $paramIDFacture = $this->adminUrlGenerator->get(ServiceCrossCanal::CROSSED_ENTITY_FACTURE);
-            if ($paramIDFacture != null) {
-                $this->paiement->setFacture(
-                    $this->entityManager->getRepository(Facture::class)->find($paramIDFacture)
-                );
-            }
-        }else{
-            $this->paiement->setFacture($facture);
-        }
-        $this->paiement->setPaidAt($dateOfPayment);
-        if ($paidAmount != 0) {
-            $this->paiement->setMontant($paidAmount);
-        } else {
-            $this->paiement->setMontant($facture->getTotalDu() - $facture->getTotalRecu());
-        }
-        $this->paiement->setType(PaiementCrudController::TAB_TYPE_PAIEMENT[PaiementCrudController::TYPE_PAIEMENT_SORTIE]);
-        $this->paiement->setTypeFacture($facture->getType());
-        $this->paiement->setDescription("Paiement de " . $this->serviceTaxes->getTaxe(false) . ". Facture n°" . $facture . ". Versement effectué le " . $this->serviceDates->getTexte($dateOfPayment));
-        $this->paiement->setEntreprise($facture->getEntreprise());
-        $this->paiement->setUtilisateur($facture->getUtilisateur());
-        $this->paiement->setCreatedAt($this->serviceDates->aujourdhui());
-        $this->paiement->setUpdatedAt($this->serviceDates->aujourdhui());
-        // dd("Fonction pas encore définie");
-        return $this->paiement;
-    }
-
-    public function loadSavedPaiements(?Facture $facture): ?array
-    {
-        // dd("Fonction non encore définie");
-        $tabPaiements = [];
-        if (count($facture->getPaiements()) != 0) {
-            /** @var Paiement */
-            foreach ($facture->getPaiements() as $paiementEnregistre) {
-                // $paiementEnregistre = $this->entityManager->getRepository(Paiement::class)->find($fact->getFacture()->getId());
-                if ($paiementEnregistre != null) {
-                    $tabPaiements[] = $paiementEnregistre;
-                }
-            }
-        }
-        return $tabPaiements;
-    }
-
-    public function savePaiement()
-    {
-        if ($this->paiement != null) {
-            $ancienPaiements = $this->loadSavedPaiements($this->facture);
-            $testEquality = $this->areEqual($ancienPaiements, $this->paiement);
-            if ($testEquality[self::PARAM_FINAL] == false) {
-                //Enregistrement du paiement
-                if (
-                    $testEquality[self::PARAM_SAME_MONTANT] == false &&
-                    $testEquality[self::PARAM_SAME_FACTURE] == true
-                ) {
-                    //On y ajoute la différence
-                    $this->paiement->setMontant(
-                        $testEquality[self::PARAM_DIFFERENCES][self::PARAM_SAME_MONTANT]
-                    );
-                    $this->paiement->setDescription("Ajustement (" . $this->serviceDates->getTexte($this->serviceDates->aujourdhui()) . ") - " . $this->paiement->getDescription());
-                    // dd("Facture à ajouter", $this->facture);
-                    $this->entityManager->persist($this->paiement);
-                } else {
-                    $this->entityManager->persist($this->paiement);
-                }
-                $this->entityManager->flush();
-            }
-        }
-    }
-
-    public function areEqual(?array $anciennesPaiements, ?Paiement $nouveauPaiement)
-    {
-        // dd("Fonction non encore définie");
-        $sameMontant = false;
-        $sameFacture = false;
-        $final = false;
-        $diff = 0;
-        if (count($anciennesPaiements) != 0 && $nouveauPaiement != null) {
-            $cumulMontantAnciennePaiements = 0;
-            /** @var Paiement */
-            foreach ($anciennesPaiements as $anciennePaiement) {
-                if ($anciennePaiement->getTypeFacture() == $nouveauPaiement->getTypeFacture()) {
-                    $cumulMontantAnciennePaiements = $cumulMontantAnciennePaiements + $nouveauPaiement->getMontant();
-                    $sameFactureTempo = $anciennePaiement->getFacture() == $nouveauPaiement->getFacture();
-                    if ($sameFactureTempo == true) {
-                        $sameFacture = $sameFactureTempo;
-                    }
-                }
-            }
-            $sameMontant = $cumulMontantAnciennePaiements == $nouveauPaiement->getMontant();
-            $final = $sameMontant == true && $sameFacture;
-            $diff = ($nouveauPaiement->getMontant() - $cumulMontantAnciennePaiements);
-        } else if (count($anciennesPaiements) == 0 && $nouveauPaiement != null) {
-            $diff = $nouveauPaiement->getMontant();
-        }
-        $reponse = [
-            self::PARAM_SAME_MONTANT => $sameMontant,
-            self::PARAM_SAME_FACTURE => $sameFacture,
-            self::PARAM_FINAL => $final,
-            self::PARAM_DIFFERENCES => [
-                self::PARAM_SAME_MONTANT => $diff,
-            ],
-        ];
-        // dd("Anciennes factures: ", count($anciennesFactures), $anciennesFactures, $reponse);
-        return $reponse;
+        return "Reglèment de ".$this->serviceTaxes->getNomTaxeAssureur()." (dû à " . $this->serviceTaxes->getTaxe(false)->getOrganisation() . ").";
     }
 }

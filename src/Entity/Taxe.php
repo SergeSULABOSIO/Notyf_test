94a3ed17bbb2f099e8bfdade7d7c592a755d5aa7
Serge - Bureau <sulabosiog@gmail.com>
2024-03-14 10:50:45 +0100

    Résolution du problème de création du pdf de la facture

--- src/Entity/Taxe.php
+++ src/Entity/Taxe.php
@@ -2,9 +2,6 @@
 
 namespace App\Entity;
 
-
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Symfony\Component\Validator\Constraints as Assert;
 use App\Repository\TaxeRepository;
 use Doctrine\DBAL\Types\Types;

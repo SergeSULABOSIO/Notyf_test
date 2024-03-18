a230e0b6c15ec7771f7a93c4a0fde31eef516164
Serge - Bureau <sulabosiog@gmail.com>
2024-03-18 11:13:11 +0100

    Résolution du problème de création du pdf de la facture - MAJ du bordereau de la ND Client

--- src/Service/RefactoringJS/Commandes/Facture/CommandeProduireArticlesGrouperSelonNotes.php
+++ src/Service/RefactoringJS/Commandes/Facture/CommandeProduireArticlesGrouperSelonNotes.php
@@ -4,6 +4,7 @@ namespace App\Service\RefactoringJS\Commandes\Facture;
 
 use App\Entity\Facture;
 use App\Controller\Admin\FactureCrudController;
+use App\Entity\Police;
 use App\Entity\Tranche;
 use App\Service\RefactoringJS\Commandes\Commande;
 
@@ -33,6 +34,9 @@ class CommandeProduireArticlesGrouperSelonNotes implements Commande
                             /** @var Tranche */
                             $tranche = $elementFacture->getTranche();
                             if ($tranche != null) {
+                                /** @var Police */
+                                $police = $tranche->getPolice();
+
                                 $primeTTC = $tranche->getPrimeTotaleTranche();
                                 $primeHt = $tranche->getPrimeNetteTranche();
                                 $taxeAssureur = $tranche->getTvaTranche();
@@ -40,9 +44,9 @@ class CommandeProduireArticlesGrouperSelonNotes implements Commande
                                 $this->notesElementsFactures[] =
                                     [
                                         "No" => $indexLigne,
-                                        "Reference_Police" => $tranche->getPolice()->getReference(),
-                                        "Avenant" => $tranche->getPolice()->getTypeavenant(),
-                                        "Risque" => $tranche->getPolice()->getProduit()->getCode(),
+                                        "Reference_Police" => $police->getReference(),
+                                        "Avenant" => $police->getTypeavenant(),
+                                        "Risque" => $police->getProduit()->getCode(),
                                         "Tranche" => $tranche->getNom(),
                                         "Note" => FactureCrudController::TYPE_NOTE_PRIME,
                                         "Prime_TTC" => $primeTTC / 100,
@@ -62,31 +66,37 @@ class CommandeProduireArticlesGrouperSelonNotes implements Commande
                          * FRAIS DE GESTION
                          */
                         if ($elementFacture->getIncludeFraisGestion() == true) {
-                            $primeTTC = $elementFacture->getTranche()->getPrimeTotaleTranche();
-                            $primeHt = $elementFacture->getTranche()->getPrimeNetteTranche();
-                            $mntTTC = $elementFacture->getMontantInvoicedPerTypeNote(FactureCrudController::TAB_TYPE_NOTE[FactureCrudController::TYPE_NOTE_FRAIS_DE_GESTION]);
-                            // $taxeCourtier = ($taxeC->getTauxIARD());
-                            $taxeCourtier = $elementFacture->getTranche()->getTaxeCourtierTotale();
-                            // dd($taxeCourtier);
-                            $mntHT = $mntTTC - $taxeCourtier;
-                            $this->notesElementsFactures[] =
-                                [
-                                    "No" => $indexLigne,
-                                    "Reference_Police" => $elementFacture->getTranche()->getPolice()->getReference(),
-                                    "Avenant" => $elementFacture->getTranche()->getPolice()->getTypeavenant(),
-                                    "Risque" => $elementFacture->getTranche()->getPolice()->getProduit()->getCode(),
-                                    "Tranche" => $elementFacture->getTranche()->getNom(),
-                                    "Note" => FactureCrudController::TYPE_NOTE_FRAIS_DE_GESTION,
-                                    "Prime_TTC" => $primeTTC / 100,
-                                    "Prime_HT" => $primeHt / 100,
-                                    "Fronting" => $elementFacture->getTranche()->getFrontingTranche() / 100,
-                                    "Taxe_Assureur" => $taxeAssureur / 100,
-                                    "Taux" => ($primeHt != 0) ? (($mntHT / $primeHt) * 100) : 0,
-                                    "Montant" => $mntHT / 100,
-                                    "Taxes" => $taxeCourtier / 100,
-                                    "Total_Dû" => $mntTTC / 100
-                                ];
-                            $indexLigne = $indexLigne + 1;
+                            /** @var Tranche */
+                            $tranche = $elementFacture->getTranche();
+                            if ($tranche != null) {
+                                /** @var Police */
+                                $police = $tranche->getPolice();
+
+                                $primeTTC = $tranche->getPrimeTotaleTranche();
+                                $primeHt = $tranche->getPrimeNetteTranche();
+                                $mntTTC = $elementFacture->getMontantInvoicedPerTypeNote(FactureCrudController::TAB_TYPE_NOTE[FactureCrudController::TYPE_NOTE_FRAIS_DE_GESTION]);
+                                $taxeCourtier = $tranche->getTaxeCourtierTotale();
+                                // dd($taxeCourtier);
+                                $mntHT = $mntTTC - $taxeCourtier;
+                                $this->notesElementsFactures[] =
+                                    [
+                                        "No" => $indexLigne,
+                                        "Reference_Police" => $police->getReference(),
+                                        "Avenant" => $police->getTypeavenant(),
+                                        "Risque" => $police->getProduit()->getCode(),
+                                        "Tranche" => $tranche->getNom(),
+                                        "Note" => FactureCrudController::TYPE_NOTE_FRAIS_DE_GESTION,
+                                        "Prime_TTC" => $primeTTC / 100,
+                                        "Prime_HT" => $primeHt / 100,
+                                        "Fronting" => $tranche->getFrontingTranche() / 100,
+                                        "Taxe_Assureur" => $taxeAssureur / 100,
+                                        "Taux" => ($primeHt != 0) ? (($mntHT / $primeHt) * 100) : 0,
+                                        "Montant" => $mntHT / 100,
+                                        "Taxes" => $taxeCourtier / 100,
+                                        "Total_Dû" => $mntTTC / 100
+                                    ];
+                                $indexLigne = $indexLigne + 1;
+                            }
                         }
                     }
                 }

1f137bf780ea1fe89f3d565c0597807749846761
Serge SULA BOSIO <sulabosiog@gmail.com>
2024-04-12 10:56:49 +0200

    Evenement...Monnaie.

--- src/Entity/Monnaie.php
+++ src/Entity/Monnaie.php
@@ -3,13 +3,21 @@
 namespace App\Entity;
 
 
-use Symfony\Component\Validator\Constraints as Assert;
-use App\Repository\MonnaieRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use App\Repository\MonnaieRepository;
+use App\Service\RefactoringJS\Evenements\Sujet;
+use Doctrine\Common\Collections\ArrayCollection;
+use App\Service\RefactoringJS\Commandes\Commande;
+use App\Service\RefactoringJS\Evenements\Evenement;
+use App\Service\RefactoringJS\Evenements\Observateur;
+use Symfony\Component\Validator\Constraints as Assert;
+use App\Service\RefactoringJS\Commandes\CommandeExecuteur;
+use App\Service\RefactoringJS\Commandes\CommandeDetecterChangementAttribut;
+use App\Service\RefactoringJS\Commandes\Piste\CommandePisteNotifierEvenement;
 
 #[ORM\Entity(repositoryClass: MonnaieRepository::class)]
-class Monnaie
+class Monnaie implements Sujet, CommandeExecuteur
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
@@ -19,11 +27,11 @@ class Monnaie
     #[ORM\Column(length: 255)]
     private ?string $nom = null;
 
-    #[Assert\NotBlank(message:"Le code ne peut pas être vide.")]
+    #[Assert\NotBlank(message: "Le code ne peut pas être vide.")]
     #[ORM\Column(length: 255)]
     private ?string $code = null;
 
-    #[Assert\NotBlank(message:"Le taux ne peut pas être vide.")]
+    #[Assert\NotBlank(message: "Le taux ne peut pas être vide.")]
     #[ORM\Column(type: Types::DECIMAL, precision: 10, scale: 2)]
     private ?string $tauxusd = null;
 
@@ -45,6 +53,15 @@ class Monnaie
     #[ORM\ManyToOne(inversedBy: 'monnaies')]
     private ?Entreprise $entreprise = null;
 
+    //Evenements
+    private ?ArrayCollection $listeObservateurs = null;
+
+
+    public function __construct()
+    {
+        $this->listeObservateurs = new ArrayCollection();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
@@ -57,7 +74,11 @@ class Monnaie
 
     public function setNom(string $nom): self
     {
+        $oldValue = $this->getNom();
+        $newValue = $nom;
         $this->nom = $nom;
+        //Ecouteur d'action
+        $this->executer(new CommandeDetecterChangementAttribut($this, "Nom", $oldValue, $newValue, Evenement::FORMAT_VALUE_PRIMITIVE));
 
         return $this;
     }
@@ -69,7 +90,11 @@ class Monnaie
 
     public function setCode(string $code): self
     {
+        $oldValue = $this->getCode();
+        $newValue = $code;
         $this->code = $code;
+        //Ecouteur d'action
+        $this->executer(new CommandeDetecterChangementAttribut($this, "Code", $oldValue, $newValue, Evenement::FORMAT_VALUE_PRIMITIVE));
 
         return $this;
     }
@@ -81,7 +106,11 @@ class Monnaie
 
     public function setTauxusd(string $tauxusd): self
     {
+        $oldValue = $this->getTauxusd();
+        $newValue = $tauxusd;
         $this->tauxusd = $tauxusd;
+        //Ecouteur d'action
+        $this->executer(new CommandeDetecterChangementAttribut($this, "Taux en USD", $oldValue, $newValue, Evenement::FORMAT_VALUE_PRIMITIVE));
 
         return $this;
     }
@@ -93,22 +122,14 @@ class Monnaie
 
     public function setIslocale(bool $islocale): self
     {
+        $oldValue = $this->isIslocale();
+        $newValue = $islocale;
         $this->islocale = $islocale;
+        //Ecouteur d'action
+        $this->executer(new CommandeDetecterChangementAttribut($this, "Monaie locale? (O/N)", $oldValue, $newValue, Evenement::FORMAT_VALUE_PRIMITIVE));
 
         return $this;
     }
-
-    // public function getEntreprise(): ?Entreprise
-    // {
-    //     return $this->entreprise;
-    // }
-
-    // public function setEntreprise(?Entreprise $entreprise): self
-    // {
-    //     $this->entreprise = $entreprise;
-
-    //     return $this;
-    // }
 
     public function __toString()
     {
@@ -158,7 +179,11 @@ class Monnaie
 
     public function setFonction(int $fonction): self
     {
+        $oldValue = $this->getFonction();
+        $newValue = $fonction;
         $this->fonction = $fonction;
+        //Ecouteur d'action
+        $this->executer(new CommandeDetecterChangementAttribut($this, "Fonction", $oldValue, $newValue, Evenement::FORMAT_VALUE_PRIMITIVE));
 
         return $this;
     }
@@ -173,5 +198,67 @@ class Monnaie
         $this->entreprise = $entreprise;
 
         return $this;
+    }
+
+
+
+
+    /**
+     * LES METHODES NECESSAIRES AUX ECOUTEURS D'ACTIONS
+     */
+
+
+    public function ajouterObservateur(?Observateur $observateur)
+    {
+        // Ajout observateur
+        $this->initListeObservateurs();
+        if (!$this->listeObservateurs->contains($observateur)) {
+            $this->listeObservateurs->add($observateur);
+        }
+    }
+
+    public function retirerObservateur(?Observateur $observateur)
+    {
+        $this->initListeObservateurs();
+        if ($this->listeObservateurs->contains($observateur)) {
+            $this->listeObservateurs->removeElement($observateur);
+        }
+    }
+
+    public function viderListeObservateurs()
+    {
+        $this->initListeObservateurs();
+        if (!$this->listeObservateurs->isEmpty()) {
+            $this->listeObservateurs = new ArrayCollection([]);
+        }
+    }
+
+    public function getListeObservateurs(): ?ArrayCollection
+    {
+        return $this->listeObservateurs;
+    }
+
+    public function setListeObservateurs(ArrayCollection $listeObservateurs)
+    {
+        $this->listeObservateurs = $listeObservateurs;
+    }
+
+    public function notifierLesObservateurs(?Evenement $evenement)
+    {
+        $this->executer(new CommandePisteNotifierEvenement($this->listeObservateurs, $evenement));
+    }
+
+    public function initListeObservateurs()
+    {
+        if ($this->listeObservateurs == null) {
+            $this->listeObservateurs = new ArrayCollection();
+        }
+    }
+
+    public function executer(?Commande $commande)
+    {
+        if ($commande != null) {
+            $commande->executer();
+        }
     }
 }

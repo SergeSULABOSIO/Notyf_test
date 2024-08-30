52a8bdc4bfbab3b166c627c390a676e64cc6bc44
Serge SULA BOSIO <sulabosiog@gmail.com>
2024-08-30 22:50:41 +0200

    UIComponents - Feedback

---
+++ src/Service/RefactoringJS/JSUIComponents/Feedback/FeedbackListeRenderer.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Service\RefactoringJS\JSUIComponents\Feedback;
+
+use App\Service\ServiceTaxes;
+use App\Service\ServiceMonnaie;
+use Doctrine\ORM\EntityManager;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use App\Controller\Admin\PreferenceCrudController;
+use App\Entity\FeedbackCRM;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use App\Service\RefactoringJS\JSUIComponents\JSUIParametres\JSChamp;
+use App\Service\RefactoringJS\JSUIComponents\JSUIParametres\JSPanelRenderer;
+use App\Service\RefactoringJS\JSUIComponents\JSUIParametres\JSCssHtmlDecoration;
+
+class FeedbackListeRenderer extends JSPanelRenderer
+{
+    public function __construct(
+        private EntityManager $entityManager,
+        private ServiceMonnaie $serviceMonnaie,
+        private ServiceTaxes $serviceTaxes,
+        private string $pageName,
+        private $objetInstance,
+        private $crud,
+        private AdminUrlGenerator $adminUrlGenerator
+    ) {
+        parent::__construct(self::TYPE_LISTE, $pageName, $objetInstance, $crud, $adminUrlGenerator);
+    }
+
+    public function design()
+    {
+        //Message
+        $this->addChamp(
+            (new JSChamp())
+                ->createTexte('Message', PreferenceCrudController::PREF_CRM_FEEDBACK_MESAGE)
+                ->setColumns(10)
+                ->getChamp()
+        );
+        //Tache / Action
+        $this->addChamp(
+            (new JSChamp())
+                ->createAssociation('actionCRM', PreferenceCrudController::PREF_CRM_FEEDBACK_ACTION)
+                ->setColumns(10)
+                ->getChamp()
+        );
+        //Date de création
+        $this->addChamp(
+            (new JSChamp())
+                ->createDate('createdAt', PreferenceCrudController::PREF_CRM_FEEDBACK_DATE_CREATION)
+                ->setColumns(10)
+                ->setFormatValue(
+                    function ($value, FeedbackCRM $objet) {
+                        /** @var JSCssHtmlDecoration */
+                        $formatedHtml = (new JSCssHtmlDecoration("span", $value))
+                            ->ajouterClasseCss($this->css_class_bage_ordinaire)
+                            ->outputHtml();
+                        return $formatedHtml;
+                    }
+                )
+                ->getChamp()
+        );
+    }
+
+    public function batchActions(?array $champs, ?string $type = null, ?string $pageName = null, $objetInstance = null, ?Crud $crud = null, ?AdminUrlGenerator $adminUrlGenerator = null): ?array
+    {
+        return $champs;
+    }
+}

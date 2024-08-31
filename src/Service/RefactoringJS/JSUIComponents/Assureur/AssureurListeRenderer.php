ab77ea284656a1a6892fa60dbe8ef9d153e62d5d
Serge SULA BOSIO <sulabosiog@gmail.com>
2024-08-31 15:38:17 +0200

    UIComponents - Assureur

---
+++ src/Service/RefactoringJS/JSUIComponents/Assureur/AssureurListeRenderer.php
@@ -0,0 +1,169 @@
+<?php
+
+namespace App\Service\RefactoringJS\JSUIComponents\Assureur;
+
+use App\Entity\Assureur;
+use App\Service\ServiceTaxes;
+use App\Service\ServiceMonnaie;
+use Doctrine\ORM\EntityManager;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use App\Controller\Admin\PreferenceCrudController;
+use App\Controller\Admin\UtilisateurCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use App\Service\RefactoringJS\JSUIComponents\JSUIParametres\JSChamp;
+use App\Service\RefactoringJS\JSUIComponents\JSUIParametres\JSPanelRenderer;
+use App\Service\RefactoringJS\JSUIComponents\JSUIParametres\JSCssHtmlDecoration;
+
+class AssureurListeRenderer extends JSPanelRenderer
+{
+    public function __construct(
+        private EntityManager $entityManager,
+        private ServiceMonnaie $serviceMonnaie,
+        private ServiceTaxes $serviceTaxes,
+        private string $pageName,
+        private $objetInstance,
+        private $crud,
+        private AdminUrlGenerator $adminUrlGenerator
+    ) {
+        parent::__construct(self::TYPE_LISTE, $pageName, $objetInstance, $crud, $adminUrlGenerator);
+    }
+
+    public function design()
+    {
+        //Nom
+        $this->addChamp(
+            (new JSChamp())
+                ->createTexte("nom", PreferenceCrudController::PREF_PRO_ASSUREUR_NOM)
+                ->setColumns(10)
+                ->getChamp()
+        );
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        // //Cotation
+        // $this->addChamp(
+        //     (new JSChamp())
+        //         ->createTableau("cotations", PreferenceCrudController::PREF_PRO_ASSUREUR_COTATIONS)
+        //         ->setTemplatePath('admin/segment/view_cotations.html.twig')
+        //         ->setColumns(10)
+        //         ->getChamp()
+        // );
+        // //Adresse
+        // $this->addChamp(
+        //     (new JSChamp())
+        //         ->createTexte('adresse', PreferenceCrudController::PREF_PRO_ASSUREUR_ADRESSE)
+        //         ->setColumns(10)
+        //         ->getChamp()
+        // );
+        // //Téléphone
+        // $this->addChamp(
+        //     (new JSChamp())
+        //         ->createTexte('telephone', PreferenceCrudController::PREF_PRO_ASSUREUR_TELEPHONE)
+        //         ->setColumns(10)
+        //         ->getChamp()
+        // );
+        // //Email
+        // $this->addChamp(
+        //     (new JSChamp())
+        //         ->createTexte('email', PreferenceCrudController::PREF_PRO_ASSUREUR_EMAIL)
+        //         ->setColumns(10)
+        //         ->getChamp()
+        // );
+        // //Site web
+        // $this->addChamp(
+        //     (new JSChamp())
+        //         ->createTexte('siteweb', PreferenceCrudController::PREF_PRO_ASSUREUR_SITE_WEB)
+        //         ->setColumns(10)
+        //         ->getChamp()
+        // );
+        // //RCCM
+        // $this->addChamp(
+        //     (new JSChamp())
+        //         ->createTexte('rccm', PreferenceCrudController::PREF_PRO_ASSUREUR_RCCM)
+        //         ->setColumns(10)
+        //         ->getChamp()
+        // );
+        // //Licence
+        // $this->addChamp(
+        //     (new JSChamp())
+        //         ->createTexte('licence', PreferenceCrudController::PREF_PRO_ASSUREUR_LICENCE)
+        //         ->setColumns(10)
+        //         ->getChamp()
+        // );
+        // //Idnat
+        // $this->addChamp(
+        //     (new JSChamp())
+        //         ->createTexte('idnat', PreferenceCrudController::PREF_PRO_ASSUREUR_IDNAT)
+        //         ->setColumns(10)
+        //         ->getChamp()
+        // );
+        // //Num Impot
+        // $this->addChamp(
+        //     (new JSChamp())
+        //         ->createTexte('numimpot', PreferenceCrudController::PREF_PRO_ASSUREUR_NUM_IMPOT)
+        //         ->setColumns(10)
+        //         ->getChamp()
+        // );
+        // //Utilisateur
+        // $this->addChamp(
+        //     (new JSChamp())
+        //         ->createAssociation("utilisateur", PreferenceCrudController::PREF_PRO_ASSUREUR_UTILISATEUR)
+        //         ->setPermission(UtilisateurCrudController::TAB_ROLES[UtilisateurCrudController::VISION_GLOBALE])
+        //         ->setColumns(10)
+        //         ->getChamp()
+        // );
+        // //Entreprise
+        // $this->addChamp(
+        //     (new JSChamp())
+        //         ->createAssociation("entreprise", PreferenceCrudController::PREF_PRO_ASSUREUR_ENTREPRISE)
+        //         ->setPermission(UtilisateurCrudController::TAB_ROLES[UtilisateurCrudController::VISION_GLOBALE])
+        //         ->setColumns(10)
+        //         ->getChamp()
+        // );
+        // //Date de création
+        // $this->addChamp(
+        //     (new JSChamp())
+        //         ->createDate("createdAt", PreferenceCrudController::PREF_PRO_ASSUREUR_DATE_DE_CREATION)
+        //         ->setColumns(10)
+        //         ->setFormatValue(
+        //             function ($value, Assureur $objet) {
+        //                 /** @var JSCssHtmlDecoration */
+        //                 $formatedHtml = (new JSCssHtmlDecoration("span", $value))
+        //                     ->ajouterClasseCss($this->css_class_bage_ordinaire)
+        //                     ->outputHtml();
+        //                 return $formatedHtml;
+        //             }
+        //         )
+        //         ->getChamp()
+        // );
+        // //Dernière modification
+        // $this->addChamp(
+        //     (new JSChamp())
+        //         ->createDate("updatedAt", PreferenceCrudController::PREF_PRO_ASSUREUR_DATE_DE_MODIFICATION)
+        //         ->setColumns(10)
+        //         ->setFormatValue(
+        //             function ($value, Assureur $objet) {
+        //                 /** @var JSCssHtmlDecoration */
+        //                 $formatedHtml = (new JSCssHtmlDecoration("span", $value))
+        //                     ->ajouterClasseCss($this->css_class_bage_ordinaire)
+        //                     ->outputHtml();
+        //                 return $formatedHtml;
+        //             }
+        //         )
+        //         ->getChamp()
+        // );
+    }
+
+    public function batchActions(?array $champs, ?string $type = null, ?string $pageName = null, $objetInstance = null, ?Crud $crud = null, ?AdminUrlGenerator $adminUrlGenerator = null): ?array
+    {
+        return $champs;
+    }
+}

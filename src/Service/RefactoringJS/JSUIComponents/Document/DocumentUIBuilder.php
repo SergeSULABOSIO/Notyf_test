9104918e2d73a31e8be03ca3201fb58302ea766b
Serge SULA BOSIO <sulabosiog@gmail.com>
2024-09-02 12:06:47 +0200

    UIComponents - Document

---
+++ src/Service/RefactoringJS/JSUIComponents/Document/DocumentUIBuilder.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Service\RefactoringJS\JSUIComponents\Document;
+
+use App\Service\ServiceTaxes;
+use App\Service\ServiceMonnaie;
+use Doctrine\ORM\EntityManager;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use App\Service\RefactoringJS\JSUIComponents\Document\DocumentFormRenderer;
+use App\Service\RefactoringJS\JSUIComponents\JSUIParametres\JSPanelBuilder;
+use App\Service\RefactoringJS\JSUIComponents\Document\DocumentListeRenderer;
+use App\Service\RefactoringJS\JSUIComponents\Document\DocumentDetailsRenderer;
+
+class DocumentUIBuilder extends JSPanelBuilder
+{
+    private ?DocumentListeRenderer $listeRendere = null;
+    private ?DocumentDetailsRenderer $detailsRendere = null;
+    private ?DocumentFormRenderer $formRendere = null;
+
+    public function __construct()
+    {
+    }
+
+    public function buildListPanel(?EntityManager $entityManager = null, ?ServiceMonnaie $serviceMonnaie = null, ?ServiceTaxes $serviceTaxes = null, ?string $pageName = null, $objetInstance = null, $crud = null, ?AdminUrlGenerator $adminUrlGenerator = null): ?array
+    {
+        $this->listeRendere = new DocumentListeRenderer(
+            $entityManager,
+            $serviceMonnaie,
+            $serviceTaxes,
+            $pageName,
+            $objetInstance,
+            $crud,
+            $adminUrlGenerator
+        );
+        return $this->listeRendere->getChamps();
+    }
+
+    public function buildFormPanel(?EntityManager $entityManager = null, ?ServiceMonnaie $serviceMonnaie = null, ?ServiceTaxes $serviceTaxes = null, ?string $pageName = null, $objetInstance = null, $crud = null, ?AdminUrlGenerator $adminUrlGenerator = null): ?array
+    {
+        $this->formRendere = new DocumentFormRenderer(
+            $entityManager,
+            $serviceMonnaie,
+            $serviceTaxes,
+            $pageName,
+            $objetInstance,
+            $crud,
+            $adminUrlGenerator
+        );
+        return $this->formRendere->getChamps();
+    }
+
+    public function buildDetailsPanel(?EntityManager $entityManager = null, ?ServiceMonnaie $serviceMonnaie = null, ?ServiceTaxes $serviceTaxes = null, ?string $pageName = null, $objetInstance = null, $crud = null, ?AdminUrlGenerator $adminUrlGenerator = null): ?array
+    {
+        $this->detailsRendere = new DocumentDetailsRenderer(
+            $entityManager,
+            $serviceMonnaie,
+            $serviceTaxes,
+            $pageName,
+            $objetInstance,
+            $crud,
+            $adminUrlGenerator
+        );
+        return $this->detailsRendere->getChamps();
+    }
+}

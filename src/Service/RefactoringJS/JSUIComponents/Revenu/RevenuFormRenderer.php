45c5044d7f5122451fc49cd80b03ae717063666c
Serge SULA BOSIO <sulabosiog@gmail.com>
2024-09-02 12:50:01 +0200

    UIComponents - Revenu

--- src/Service/RefactoringJS/JSUIComponents/Revenu/RevenuFormRenderer.php
+++ src/Service/RefactoringJS/JSUIComponents/Revenu/RevenuFormRenderer.php
@@ -3,15 +3,12 @@
 namespace App\Service\RefactoringJS\JSUIComponents\Revenu;
 
 use App\Entity\Revenu;
-use App\Entity\Tranche;
 use App\Service\ServiceTaxes;
 use App\Service\ServiceMonnaie;
 use Doctrine\ORM\EntityManager;
 use App\Controller\Admin\RevenuCrudController;
-use App\Controller\Admin\PaiementCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use App\Controller\Admin\PreferenceCrudController;
-use App\Controller\Admin\UtilisateurCrudController;
 use App\Service\RefactoringJS\JSUIComponents\JSUIParametres\JSChamp;
 use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use App\Service\RefactoringJS\JSUIComponents\JSUIParametres\JSPanelRenderer;
@@ -23,41 +20,46 @@ class RevenuFormRenderer extends JSPanelRenderer
         private EntityManager $entityManager,
         private ServiceMonnaie $serviceMonnaie,
         private ServiceTaxes $serviceTaxes,
-        string $pageName,
-        $objetInstance,
-        $crud,
-        AdminUrlGenerator $adminUrlGenerator
+        private string $pageName,
+        private $objetInstance,
+        private $crud,
+        private AdminUrlGenerator $adminUrlGenerator
     ) {
         parent::__construct(self::TYPE_FORMULAIRE, $pageName, $objetInstance, $crud, $adminUrlGenerator);
     }
 
     public function design()
     {
+        $column = 12;
+        if ($this->objetInstance instanceof Revenu) {
+            $column = 10;
+        }
+
+        //Section - Principale
+        $this->addChamp(
+            (new JSChamp())
+                ->createSection("Informations générales")
+                ->setIcon('fa-solid fa-burger') //<i class="fa-solid fa-burger"></i>
+                ->setHelp("Votre revenu.")
+                ->setColumns($column)
+                ->getChamp()
+        );
+
         //Type
         $this->addChamp(
             (new JSChamp())
                 ->createChoix("type", "Type de revenu")
-                ->setColumns(12)
+                ->setColumns($column)
                 ->setRequired(false)
                 ->setDisabled(false)
                 ->setChoices(RevenuCrudController::TAB_TYPE)
                 ->getChamp()
         );
-        // $this->addChampChoix(
-        //     null,
-        //     "type",
-        //     "Type de revenu",
-        //     false,
-        //     false,
-        //     12,
-        //     RevenuCrudController::TAB_TYPE,
-        //     null
-        // );
         //Partageable?
         $this->addChamp(
             (new JSChamp())
                 ->createChoix("partageable", "Partageable?")
-                ->setColumns(12)
+                ->setColumns($column)
                 ->setRequired(false)
                 ->setDisabled(false)
                 ->setChoices(RevenuCrudController::TAB_PARTAGEABLE)
@@ -69,98 +71,51 @@ class RevenuFormRenderer extends JSPanelRenderer
                 )
                 ->getChamp()
         );
-        // $this->addChampChoix(
-        //     null,
-        //     "partageable",
-        //     "Partageable?",
-        //     false,
-        //     false,
-        //     12,
-        //     RevenuCrudController::TAB_PARTAGEABLE,
-        //     [
-        //         RevenuCrudController::TAB_PARTAGEABLE[RevenuCrudController::PARTAGEABLE_NON] => 'dark',
-        //         RevenuCrudController::TAB_PARTAGEABLE[RevenuCrudController::PARTAGEABLE_OUI] => 'success',
-        //     ]
-        // );
         //Taxable?
         $this->addChamp(
             (new JSChamp())
                 ->createChoix("taxable", "Taxable?")
-                ->setColumns(12)
+                ->setColumns($column)
                 ->setRequired(false)
                 ->setDisabled(false)
                 ->setChoices(RevenuCrudController::TAB_TAXABLE)
                 ->getChamp()
         );
-        // $this->addChampChoix(
-        //     null,
-        //     "taxable",
-        //     "Taxable?",
-        //     false,
-        //     false,
-        //     12,
-        //     RevenuCrudController::TAB_TAXABLE,
-        //     null
-        // );
         //Is part Tranche
         $this->addChamp(
             (new JSChamp())
                 ->createBoolean("isparttranche", PreferenceCrudController::PREF_FIN_REVENU_PAR_TRANCHE)
+                ->setColumns($column)
                 ->setRequired(true)
                 ->setDisabled(false)
                 ->getChamp()
         );
-        // $this->addChampBooleen(
-        //     null,
-        //     "isparttranche",
-        //     PreferenceCrudController::PREF_FIN_REVENU_PAR_TRANCHE,
-        //     true,
-        //     false,
-        //     true
-        // );
         //Is part Tranche
         $this->addChamp(
             (new JSChamp())
                 ->createBoolean("ispartclient", PreferenceCrudController::PREF_FIN_REVENU_PAR_CLIENT)
+                ->setColumns($column)
                 ->setRequired(true)
                 ->setDisabled(false)
                 ->getChamp()
         );
-        // $this->addChampBooleen(
-        //     null,
-        //     "ispartclient",
-        //     PreferenceCrudController::PREF_FIN_REVENU_PAR_CLIENT,
-        //     true,
-        //     false,
-        //     true
-        // );
         //Base
         $this->addChamp(
             (new JSChamp())
                 ->createChoix("base", "Formuale")
-                ->setColumns(12)
+                ->setColumns($column)
                 ->setRequired(false)
                 ->setDisabled(false)
                 ->setChoices(RevenuCrudController::TAB_BASE)
                 ->getChamp()
         );
-        // $this->addChampChoix(
-        //     null,
-        //     "base",
-        //     "Formuale",
-        //     false,
-        //     false,
-        //     12,
-        //     RevenuCrudController::TAB_BASE,
-        //     null
-        // );
         //Taux
         $this->addChamp(
             (new JSChamp())
                 ->createPourcentage("taux", "Taux")
                 ->setRequired(false)
                 ->setDisabled(false)
-                ->setColumns(12)
+                ->setColumns($column)
                 ->setFormatValue(
                     function ($value, Revenu $objet) {
                         /** @var JSCssHtmlDecoration */
@@ -172,28 +127,13 @@ class RevenuFormRenderer extends JSPanelRenderer
                 )
                 ->getChamp()
         );
-        // $this->addChampPourcentage(
-        //     null,
-        //     "taux",
-        //     "Taux",
-        //     false,
-        //     false,
-        //     12,
-        //     function ($value, Revenu $objet) {
-        //         /** @var JSCssHtmlDecoration */
-        //         $formatedHtml = (new JSCssHtmlDecoration("span", $value))
-        //             ->ajouterClasseCss($this->css_class_bage_ordinaire)
-        //             ->outputHtml();
-        //         return $formatedHtml;
-        //     }
-        // );
         //Montant flat
         $this->addChamp(
             (new JSChamp())
                 ->createArgent("montantFlat", "Montant fixe")
                 ->setRequired(false)
                 ->setDisabled(false)
-                ->setColumns(12)
+                ->setColumns($column)
                 ->setCurrency($this->serviceMonnaie->getCodeAffichage())
                 ->setFormatValue(
                     function ($value, Revenu $objet) {
@@ -206,22 +146,6 @@ class RevenuFormRenderer extends JSPanelRenderer
                 )
                 ->getChamp()
         );
-        // $this->addChampArgent(
-        //     null,
-        //     "montantFlat",
-        //     "Montant fixe",
-        //     false,
-        //     false,
-        //     12,
-        //     $this->serviceMonnaie->getCodeAffichage(),
-        //     function ($value, Revenu $objet) {
-        //         /** @var JSCssHtmlDecoration */
-        //         $formatedHtml = (new JSCssHtmlDecoration("span", $this->serviceMonnaie->getMonantEnMonnaieAffichage($objet->getMontantFlat() * 100)))
-        //             ->ajouterClasseCss($this->css_class_bage_ordinaire)
-        //             ->outputHtml();
-        //         return $formatedHtml;
-        //     }
-        // );
     }
 
     public function batchActions(?array $champs, ?string $type = null, ?string $pageName = null, $objetInstance = null, ?Crud $crud = null, ?AdminUrlGenerator $adminUrlGenerator = null): ?array

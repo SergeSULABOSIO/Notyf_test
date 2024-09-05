9cd5cbb796f9e60f7e11320e1cc7da386c1b8249
Serge SULA BOSIO <sulabosiog@gmail.com>
2024-09-05 17:36:57 +0200

    UIComponents - Produit

---
+++ src/Service/RefactoringJS/JSUIComponents/Produit/ProduitUIBuilder.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Service\RefactoringJS\JSUIComponents\Produit;
+
+use App\Service\ServiceTaxes;
+use App\Service\ServiceMonnaie;
+use Doctrine\ORM\EntityManager;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use App\Service\RefactoringJS\JSUIComponents\JSUIParametres\JSPanelBuilder;
+
+class PartenaireUIBuilder extends JSPanelBuilder
+{
+    private ?ProduitListeRenderer $listeRendere = null;
+    private ?ProduitDetailsRenderer $detailsRendere = null;
+    private ?ProduitFormRenderer $formRendere = null;
+
+    public function __construct()
+    {
+    }
+
+    public function buildListPanel(?EntityManager $entityManager = null, ?ServiceMonnaie $serviceMonnaie = null, ?ServiceTaxes $serviceTaxes = null, ?string $pageName = null, $objetInstance = null, $crud = null, ?AdminUrlGenerator $adminUrlGenerator = null): ?array
+    {
+        $this->listeRendere = new ProduitListeRenderer(
+            $entityManager,
+            $serviceMonnaie,
+            $serviceTaxes,
+            $pageName,
+            $objetInstance,
+            $crud,
+            $adminUrlGenerator
+        );
+        return $this->listeRendere->getChamps();
+    }
+
+    public function buildFormPanel(?EntityManager $entityManager = null, ?ServiceMonnaie $serviceMonnaie = null, ?ServiceTaxes $serviceTaxes = null, ?string $pageName = null, $objetInstance = null, $crud = null, ?AdminUrlGenerator $adminUrlGenerator = null): ?array
+    {
+        $this->formRendere = new ProduitFormRenderer(
+            $entityManager,
+            $serviceMonnaie,
+            $serviceTaxes,
+            $pageName,
+            $objetInstance,
+            $crud,
+            $adminUrlGenerator
+        );
+        return $this->formRendere->getChamps();
+    }
+
+    public function buildDetailsPanel(?EntityManager $entityManager = null, ?ServiceMonnaie $serviceMonnaie = null, ?ServiceTaxes $serviceTaxes = null, ?string $pageName = null, $objetInstance = null, $crud = null, ?AdminUrlGenerator $adminUrlGenerator = null): ?array
+    {
+        $this->detailsRendere = new ProduitDetailsRenderer(
+            $entityManager,
+            $serviceMonnaie,
+            $serviceTaxes,
+            $pageName,
+            $objetInstance,
+            $crud,
+            $adminUrlGenerator
+        );
+        return $this->detailsRendere->getChamps();
+    }
+}

b87e9d40962a5dfb99d4eb39f2eb41c516e4676f
Serge SULA BOSIO <sulabosiog@gmail.com>
2024-09-11 21:47:14 +0200

    Construction du tableau de bord

--- src/Service/RefactoringJS/TableauDeBord/Interfaces/InterfaceTableauDeBaord.php
+++ src/Service/RefactoringJS/TableauDeBord/Interfaces/InterfaceTableauDeBaord.php
@@ -2,7 +2,12 @@
 
 namespace App\Service\RefactoringJS\TableauDeBord\Interfaces;
 
+use Doctrine\Common\Collections\Collection;
 
-interface InterfaceTableauDeBord {
-    
+interface InterfaceTableauDeBord
+{
+    public function addBrique(InterfaceBrique $newBrique): InterfaceTableauDeBord;
+    public function removeBrique(InterfaceBrique $brique): InterfaceTableauDeBord;
+    public function removeAllBriques(): InterfaceTableauDeBord;
+    public function getBriques(): Collection;
 }

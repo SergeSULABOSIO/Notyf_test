f65c9f038f3b16ddc561094ed89c0639bb6b6aa0
Serge SULA BOSIO <sulabosiog@gmail.com>
2024-09-12 09:08:06 +0200

    Construction du tableau de bord

--- src/Service/RefactoringJS/TableauDeBord/Abstractions/AbstractBrique.php
+++ src/Service/RefactoringJS/TableauDeBord/Abstractions/AbstractBrique.php
@@ -4,13 +4,8 @@ namespace App\Service\RefactoringJS\TableauDeBord\Interfaces;
 
 use Doctrine\Common\Collections\Collection;
 
-interface InterfaceBrique
+abstract class AbstractBrique
 {
-    public const TYPE_BRIQUE_DE_TITRE = 0;
-    public const TYPE_BRIQUE_DE_PIED = 1;
-    public const TYPE_BRIQUE_DE_GAUCHE = 2;
-    public const TYPE_BRIQUE_DE_DROITE = 3;
-
     public function addIndicateur(InterfaceIndicateur $newIndicateur): InterfaceBrique;
     public function removeIndicateur(InterfaceIndicateur $newIndicateur): InterfaceBrique;
     public function removeAllIndicateurs(): InterfaceBrique;

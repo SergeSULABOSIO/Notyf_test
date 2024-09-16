1792c6f645a0e66dab17f4c2a52348f296436a1b
Serge SULA BOSIO <sulabosiog@gmail.com>
2024-09-16 12:58:08 +0200

    Construction du tableau de bord

--- src/Controller/TableauDeBordCourtierController.php
+++ src/Controller/TableauDeBordCourtierController.php
@@ -16,7 +16,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class TableauDeBordCourtierController extends AbstractController
 {
-
     public function __construct(
         private ChartBuilderInterface $chartBuilder,
         private ServicePreferences $servicePreferences,
---
+++ src/Service/RefactoringJS/TableauDeBord/Composants/Alert.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Service\RefactoringJS\TableauDeBord\Composants;
+
+class Alert
+{
+    public string $type = "success";
+    public string $message;
+}

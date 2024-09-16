684ff37b98047a4f0495bd7fa8e97aa212379dd5
Serge SULA BOSIO <sulabosiog@gmail.com>
2024-09-16 16:29:25 +0200

    Construction du tableau de bord

--- src/Twig/Components/Alert.php
+++ src/Twig/Components/Alert.php
@@ -1,12 +1,25 @@
 <?php
 namespace App\Twig\Components;
 
+use App\Repository\PisteRepository;
 use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
 
 #[AsTwigComponent]
 class Alert
 {
+    public function __construct(
+        private PisteRepository $pisteRepository
+    )
+    {
+        
+    }
     public string $type = "success";
     public string $titre;
     public string $message;
+
+    public function getPistes():array
+    {
+        // dd($this->pisteRepository->findAll());
+        return $this->pisteRepository->findAll();
+    }
 }

6dca101b70a27933fdb2ebdd422fcd8f3ad6252f
Serge SULA BOSIO <sulabosiog@gmail.com>
2024-09-17 10:48:42 +0200

    Construction du tableau de bord

--- src/Twig/Components/Section.php
+++ src/Twig/Components/Section.php
@@ -14,4 +14,16 @@ class Section
     public const TYPE_DU_BAS = 2;
 
     public function __construct() {}
+
+    public function getDescriptionType(): string
+    {
+        switch ($this->type) {
+            case self::TYPE_DE_HAUT:
+                return "Section du haut de la page";
+            case self::TYPE_DU_CENTRE:
+                return "Section du centre de la page";
+            case self::TYPE_DU_BAS:
+                return "Section du base de la page";
+        }
+    }
 }
